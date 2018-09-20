e880873119ee2df995dfa589eb74c03117316da2
Irina <43418144+ZarIra@users.noreply.github.com>
2018-09-20 13:05:25 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -11,16 +11,21 @@ def calculate_frequences() -> dict:
     """
     stroka.lower()
     stroka.split(' ') 
-    return {'hello: 5'}
+    return stroka
 
 def filter_stop_words() -> dict:
     """
     Removes all stop words from the given frequencies dictionary
     """
+    spisok1=[1, 2, 3, 4]
+    spisok2=[3, 4, 5, 6]
+    spisok3_itog=
+    
     pass
 
 def get_top_n() -> tuple:
     """
     Takes first N popular words
     """
+    list=['dog:', '"was', 'brown,']
     pass
