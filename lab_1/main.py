35ee37391539dd7b461da66f0b3a4771d377b143
captaintb <43381620+captaintb@users.noreply.github.com>
2018-09-21 08:40:57 +0300

    add first function

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,13 +3,27 @@ Labour work #1
 
 Count frequencies dictionary by the given arbitrary text
 """
+import re
 
-
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    print (frequency)
+def calculate_frequences(text) -> dict:
+    
+    text = re.sub(r'[^\w\s]+|[\d]+', '', text)
+    if text = '':
+        print ({})
+    else:
+        text = text.lower()
+        text = text.split(' ')
+        frequency = {}
+        for i in text:
+            if i not in frequency:
+                if i != '':
+                    frequency[i] = 1
+            else:
+                num = frequency.get(i)
+                frequency[i] = num+1
+        filter_stop_words(frequency, stop_words)
+        get_top_n(frequency, top_n)
+        
     pass
 
 def filter_stop_words() -> dict:
