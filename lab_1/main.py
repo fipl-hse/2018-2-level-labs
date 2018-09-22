5fcde02b27ebc1b9a9d557c9d0831eab7879da45
Irina <43418144+ZarIra@users.noreply.github.com>
2018-09-22 23:59:05 +0300

    Update main.py
    calculate_frequences function is done

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,30 +1,42 @@
-""""
+"""
 Labour work #1
 
 Count frequencies dictionary by the given arbitrary text
 """
-text = '''The semantic field of any town is an interesting and diversified totality of different onyms which have a certain meaning given by a nominator. At the same time such system is strictly territorial, therefore it should be considered with its historical, ethnic, natural and other features of the area. The special characteristic of these objects is the social significance because they are the crucial reference point for a person in the urban environment. Onomastics is one of the sections of lexicology which studies onyms, the history of their emergence and transformations as a result of the long usage in the source language or in connection with their borrowing.
-Ergonomics is the special branch of onomastics. Ergonyms are the researching objects of ergonomics. They mean names of various business associations of people.  For the first time this term was declared by N.V. Podolskaya in “The Dictionary of Russian Onomastic Terminology”. Thanks to it, this notion has firmly consolidated in onomastics.  Ergonomic lexis as any system of onyms is characterized with a number of features.'''
-stopwords = ['onomastics', 'the', 'onym']
-calculate_frequency(text)
-filter_stop_words(frequent_dict, stopwords)
-get_top_n(frequent_dict, n)
 
-
-def calculate_frequences() -> dict:
-        """
-        Calculates number of times each word appears in the text
-        """
-        pass
+def calculate_frequences(text) -> dict: # Calculates number of times each word appears in the text
+    frequent_dict = {}
+    import re
+    prepared_text = re.findall(r'[a-z]+', text.lower())
+    print(prepared_text)
+    for index, element in enumerate(prepared_text):
+        new_word = element
+        count = 1
+        if new_word not in frequent_dict:
+            frequent_dict[new_word] = count
+        else:
+            new_count = frequent_dict[new_word] + 1
+            frequent_dict[new_word] = new_count
+    return frequent_dict
 
 def filter_stop_words() -> dict:
-        """
-        Removes all stop words from the given frequencies dictionary
-        """
-        pass
+    """
+    Removes all stop words from the given frequencies dictionary
+    """
+    pass
 
 def get_top_n() -> tuple:
-        """
-        Takes first N popular words
-        """
-        pass
+    """
+    Takes first N popular words
+    """
+    pass
+
+text = '''The the the the
+semantic field of any town is an associations of people. For the N.V. Podolskaya in “The Dictionary of Russian
+Onomastic Terminology”. 
+any system of onyms is characterized with a number of features.'''
+stopwords = ['onomastics', 'the', 'onym']
+frequent_dict = calculate_frequences(text)
+# filter_stop_words(frequent_dict, stopwords)
+# get_top_n(frequent_dict, n)
+print(frequent_dict)
