7d1cc85ebe13f63ff47ab691e1a03eac1ec19a52
ailinakw <43433103+ailinakw@users.noreply.github.com>
2018-09-23 00:05:44 +0300

    function calculate_frequences added

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,11 +5,25 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 
-def calculate_frequences() -> dict:
+def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    dict{}
+        if type(text) is str:
+        if text == '' or text is None:
+            freq_dict = {}
+        else:
+            text = text.lower()
+            for letter in text:
+                if letter not in 'abcdefghijklmnopqrstuvwxyz':
+                    text = text.replace(letter, ' ')
+            text_list = text.split()
+            freq_dict = {}
+            for word in text_list:
+                freq_dict[word] = text_list.count(word)
+    else:
+        freq_dict = {}
+    return freq_dict
     pass
 
 def filter_stop_words() -> dict:
