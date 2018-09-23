dd3a87279365db975baf9e7d893aae4b1db6557e
KsenyaSH <43419770+KsenyaSH@users.noreply.github.com>
2018-09-23 18:05:57 +0300

    Added functions

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,22 +3,66 @@ Labour work #1
 
 Count frequencies dictionary by the given arbitrary text
 """
+def print_dictionary(d: dict):
+    for b, d in d.items():
+        print(b, d)
 
-
-def calculate_frequences() -> dict:
+def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    return {'cat': 5}
+    words = text.split(' ')
+    frequencies = {}
+    for w in words:
+        if w in frequencies.keys():
+            n = frequencies[w]
+            frequencies[w] = n+1
+        else:
+            frequencies[w] = 1
 
-def filter_stop_words() -> dict:
+    return frequencies
+
+frequencies = calculate_frequences(text)
+print('Частотный словарь')
+print_dictionary(frequencies)
+
+def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
     Removes all stop words from the given frequencies dictionary
     """
-    pass
+    dct1 = dict(frequencies)
+    for k, v in dct1.items():
+        if k in stop_words:
+            del frequencies[k]
+    return dct1
 
-def get_top_n() -> tuple:
+clear_dct = filter_stop_words(frequencies, stop_words)
+print('Частотный словарь чистый')
+print_dictionary(clear_dct)
+
+def get_top_n(frequencies: dict, top_n: int) -> tuple:
     """
     Takes first N popular words
     """
-    pass
+   res = []
+    l = list(frequencies.values())
+    l.sort()
+    l.reverse()
+    for i in range(len(l)-top_n):
+        l.pop()
+    print(l)
+
+    for list_el in l:
+        for k,v in frequencies.items():
+            if v == list_el:
+                res.append(k)
+                del frequencies[k]
+                break
+
+    res = tuple(res)
+    print(res)
+    return res
+
+print('Первые', n, 'по популярности слов')
+get_top_n(frequencies, n)
+
