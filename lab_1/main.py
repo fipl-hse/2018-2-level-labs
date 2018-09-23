39c527f80c7e1c66d245c31ae871fadb60e7b1d7
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-09-23 23:26:30 +0300

    Update main.py
    add 1 part

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,17 +5,29 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
-    return ('word': 8)
+def calculate_frequences(texts: str) -> dict:
+    my_first_dict = {}
+   # texts = ''
+    if texts == '' or texts is None:
+        texts = texts.lower()
+        for bykva in texts:
+            if bykva not in 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфчцчшщъыьэюя':
+                texts = texts.replace(bykva, '')
+        texts = texts.split('')
+        if type(texts) == str:
+            for slovo in texts:
+                count_slovo = texts.count(slovo)
+                my_first_dict[slovo] = count_slovo
+            return my_first_dict
+        else:
+            return {}
 
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
+def filter_stop_words(my_first_dict: dict, stop_words: spisok) -> dict:
+
+    if not my_first_dict:
+        return my_first_dict
+
+    my_second_dict = my_first_dict.copy()
     pass
 
 def get_top_n() -> tuple:
