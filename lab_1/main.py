80885892ff3003f11a1ff6841e689d913caae6f4
Thoola <creizu34@gmail.com>
2018-09-23 15:52:22 +0300

    Body program without input and output .txt files.

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,24 +1,38 @@
-"""
-Labour work #1
+import re
+from string import punctuation
+from collections import Counter
+r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
+def stpw(x):
+    if len(x) <= 3:
+        return False
+    else:
+        return True
 
-Count frequencies dictionary by the given arbitrary text
-"""
+def file_import():
+    file = open("texte.txt", "r")
+    dbfile = file.read()
+    dbfile = r.split(dbfile)
+    stopwords()
 
-
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
-
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
-
-def get_top_n() -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+def stopwords():
+    top = int(input('top what?'))
+    words = []
+    file = open("texte.txt", "r")
+    dbfile = file.read()
+    dbfile = r.split(dbfile)
+    stopwords1 = dbfile
+    swfilter = filter(stpw, stopwords1)
+    for x in swfilter:
+        words.append(x)
+    words = [y for y in words if not (y.isdigit() 
+                                         or y[0] == '-' and y[1:].isdigit())]
+    #print (words)
+    freqfin = []
+    words = (Counter(words).most_common(top))
+    freqfin = [str(z) for z in words]
+    freqfin = [z + '\n' for z in freqfin]
+    freqfin = ''.join(freqfin)
+    freqtop = open("frequency.txt", "w")
+    freqtop.writelines (freqfin)
+    print (freqfin)
+file_import()
