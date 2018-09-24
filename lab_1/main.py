553d055504add0664a0af602220bf68c7a6b75b2
alinaalborova <43433331+alinaalborova@users.noreply.github.com>
2018-09-24 21:52:33 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,27 +6,31 @@ Count frequencies dictionary by the given arbitrary text
 
 
 def calculate_frequences(text):
-    text = ''''''
-    if text != None:
+    text = None
+    frequencies = {}
+    if text != None and type(text) != int:
         new_text = text.lower()
         alphabet = 'qwertyuiopasdfghjklzxcvbnm'
         clean_text = ''
-        i = 0
-        for i in range(len(new_text)):
-            if new_text[i] not in alphabet:
-                clean_text = new_text.replace(text[i], ' ')
-                new_text = clean_text
-        clean_list = []
-        clean_list = clean_text.split(' ')
-        frequencies = {}
-        frequencies = {token: clean_list.count(token) for token in clean_list}
-        frequencies.pop('')
-        return frequencies
+        if ' ' not in text:
+             clean_list = text.split(None)
+             frequencies = {token: clean_list.count(token) for token in clean_list}
+             return frequencies
+        else:
+             i = 0
+             for i in range(len(new_text)):
+                 if new_text[i] not in alphabet:
+                     clean_text = new_text.replace(text[i], ' ')
+                     new_text = clean_text
+             clean_list = []
+             clean_list = clean_text.split(' ')
+             frequencies = {token: clean_list.count(token) for token in clean_list}
+             frequencies.pop('')
+             return frequencies
     else:
         frequencies = {}
         return frequencies
 
-calculate_frequences()
 
 
 
