7ee7b083a8be9c889d543ac1934ddb9562f9d425
captaintb <43381620+captaintb@users.noreply.github.com>
2018-09-24 13:33:32 +0300

    Final edition

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,28 +1,32 @@
-import re
-
-text = input('')
-stop_words = input('')
+f = open('text.txt', 'r')
+text = f.read()
+f.close()
 
 
 def calculate_frequences(text) -> dict:
-    text_wos = re.sub(r'[^\w\s]+|[\d]+', '', text)
-    text_l = text_wos.lower()
-    if text_l == '':
-        print({})
-    else:
-        text_split = text_l.split(' ')
-        frequency = {}
-        for i in text_split:
-            if i not in frequency:
-                frequency[i] = 1
-            else:
-                num = frequency.get(i)
-                frequency[i] = num+1
-        if '' in frequency:
-            del frequency['']
-         return frequency
-        filter_stop_words(frequency, stop_words)
-        get_top_n()
+    punctuation = [',', '.', '<', '>', '/', '?', ';', ':', '*', '`', '~', '!',
+                   '#', '$', '%', '^', '(', ')',
+                   '-', '_', '@', '+', '{', '[', '}', ']', '|']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    for i in text:
+        if '\n' in i:
+            text = text.replace('\n', ' ')
+    for i in text:
+        if i in punctuation or i in numbers:
+            text = text.replace(i, '')
+    text_l = text.lower()
+    text_split = text_l.split(' ')
+    frequency = {}
+    for i in text_split:
+        if i not in frequency:
+            frequency[i] = 1
+        else:
+            num = frequency.get(i)
+            frequency[i] = num+1
+    stop_words = input('Please, input the stop words ')
+    top_n = int(input('Please, input how many top used words you want to see '))
+    filter_stop_words(frequency, stop_words)
+    get_top_n(frequency, top_n)
     pass
 
 
@@ -39,13 +43,20 @@ def filter_stop_words(frequency, stop_words) -> dict:
 
 
 def get_top_n() -> tuple:
-    result = collections.Counter(frequency).most_common(top_n)
-    i = 0
+    freq_list = list(frequency.items())
+    freq_sort = sorted(freq_list, key=lambda x: x[1], reverse=True)
+    count = 0
     top = []
-    while i < top_n or i != len(result):
-        top.append(max(result, key=lambda x: x[1])[0])
-        result.remove(max(result, key=lambda x: x[1]))
-        i += 1
-    top_tuple = tuple(top)
-    return top_tuple
+    for i in freq_sort:
+        if count == top_n:
+            break
+        else:
+            top.append(i[0])
+            count += 1
+    top = tuple(top)
+    file = open('result.txt', 'w')
+    for i in top:
+        file.write(i)
+        file.write('\n')
+    file.close()
     pass
