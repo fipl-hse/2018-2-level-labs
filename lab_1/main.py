b57417c8850bfa59cf0230d57d448f48e6282fa4
Irina <43418144+ZarIra@users.noreply.github.com>
2018-09-24 17:20:22 +0300

    Update main.py
    get top new

--- lab_1/main.py
+++ lab_1/main.py
@@ -56,8 +56,9 @@ def get_top_n(frequent_dict, top_n) -> tuple:
                     repository_words = words[i]
                     words[i] = words[i + 1]
                     words[i + 1] = repository_words
-        for i in range(top_n):
-            tuple_top_n = tuple_top_n[:] + (words[i],)
+         for i in range(top_n):
+            if i <= (len(words) - 1):
+                tuple_top_n = tuple_top_n[:] + (words[i],)
         print(words)
         print(frequencies)
         return tuple_top_n
