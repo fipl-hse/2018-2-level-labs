3276818135c181ae2d8d01a156d6839ddef45a5d
kateshka <43417992+kateshka@users.noreply.github.com>
2018-09-24 22:56:35 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,75 +1,83 @@
-our_file = open('data.txt', 'r')
-text = our_file.read()
-our_file.close()
-stop_words = input('Input stop words: \n')
-top_n = int(input('How many top used words you want to see: \n'))
+def read_from_file(path_to_file, lines_limit):
+    a = open(path_to_file, 'r')
+    x = 0
+    text = '' 
+    for i in a.read():
+        if x == lines_limit:
+            return text
+        else:
+            text += i
+            x += i
+            a.close()
+    return text
 
-def count_frequency(text: str) -> dict:
 
-    text = text.lower()
+def count_frequences(text):
+    
+    if text.isdigit() or text is None:
+        return {}
+    
+    punct_numb = ''',<>./"?:;}{[]!@(#$%^&*+-|№~`–_—)1234567890'''
 
     for i in text:
-        if '\n' in i:
-            text = text.replace('n', ' ')
-
-    punctuation = [',', '<', '>', '.', '/', '"', '?', ':', ';', '}', '{',
-                   '[', ']', '!', '@', '(', '#', '$', '%', '^', '&', '*',
-                   '+', '-', '|', '№', '~', '`', '–', '_', '—', ')']
-    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    for i in text:
-        if i in punctuation or i in numbers:
+        if i in punct_numb:
             text = text.replace(i, '')
 
-    text_list = text.split(' ')
+    text_down = text.lower()
+
+    text_list = text_down.split(' ')
 
     frequency = dict()
-    for word in text_list:
-        if word not in frequency:
-            frequency[word] = 1
-        else:
-            number = frequency.get(word)
-            frequency[word] = number + 1
-
-    frequency_ws = frequency.copy()
-    for key in frequency_ws.keys():
-        if key == '':
-            del frequency[key]
-
+    for i in text_list:
+        word_numb = text.count(i)
+        frequency[i] = word_numb
+        continue
     return frequency
-    filter_stop_words(frequency, stop_words)
-    get_top_n(frequencies)
 
-
-def filter_stop_words(frequency: dict, stop_words: str) -> dict:
-
-    stop_words = stop_words.lower()
-    stop_words_list = stop_words.split(' ')
-    stop_words_tuple = tuple(stop_words_list)
+    
+def filter_stop_words(frequency, stop_words):
+    
+    if frequency is None or stop_words is None:
+        return frequency
+    
+    frequency = list(frequency)
 
     frequencies = frequency.copy()
-    for i in frequency:
-        for n in stop_words_tuple:
-            if i == n:
-                del frequencies[i]
-
+    for key in frequencies:
+        if str(key).isdigit() or key in stop_words:
+            frequencies.pop(key)
+            continue
     return frequencies
 
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    frequencies_list = list(frequencies.items())
-    frequencies_sort = sorted(frequencies_list, key=lambda x: x[1], reverse=True)
-
-    result = []
-    x = 0
+def get_top_n(frequencies, top_n):
+    
+    if top_n < 0:
+        return ()
+    
+    x = top_n
+    top = []
+    frequencies_list = []
+    for key, value in frequencies.items():
+        frequencies_list.append([key, value])
+    
+    frequencies_sort = sorted(frequencies_list, reverse=True)    
     for i in frequencies_sort:
-        if x == top_n:
+        if x == 0:
             break
         else:
-            result.append(i[0])
-            x += 1
+            top.append(i[0])
+            x -= 1
+    
+    top = tuple(top)
+    return top
 
-    result = tuple(result)
-    print(result)
 
-
-count_frequency(text)
+def write_in_file(path_to_report, top):
+    
+    file = open(path_to_report, 'w')
+    
+    for i in top:
+        file.write(i)
+        file.write('\n')
+    file.close()
