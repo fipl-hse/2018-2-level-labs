dbf99bc781d275742007b42bf36416ef05dfe6c5
katvyaz <kalyaevarina@mail.ru>
2018-09-24 16:30:45 +0300

    Added code listing for testing

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,21 +4,64 @@ Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
 
+# 0: входные данные
+text = input('enter your text: \n')
+text = text.lower()
+text = text.split(' ')  # теперь это список из слов в тексте (с повторами)
+stopwords = ("actually", "like", "the", "at", "or", "sunday", "place")
+print(text)
 
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    return {'abc': 'letters', '123': 'numbers'}
+# 1: функция, создающая частотный словарь
 
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
+def calculate_frequences(text):
 
-def get_top_n() -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+    for word in text:
+        if word in freq_dict:
+            freq_dict[word] += 1
+        else:
+            freq_dict[word] = 1
+
+    return freq_dict
+
+# 2: очистка словаря от стоп-слов
+
+def filter_stop_words(freq_dict_clean, stopwords):
+
+    print(freq_dict_clean)
+
+    for key in list(freq_dict_clean):
+        if key in stopwords:
+            del freq_dict_clean[key]
+    print(freq_dict_clean) ### del
+
+    return freq_dict_clean
+
+# 3: выстраивание топ-n  самых частых слов
+
+def get_top_n(freq_dict_clean, top_n):
+
+    #top_n = int(input('enter n for top_n: \n'))
+
+    for k, v in freq_dict_clean.items():  # здесь нужно пройти по парам ключ-значение в словаре и накопить их в список (вложенные списки)
+        freq_list.append([v,k])
+    print(freq_list)
+    freq_list.sort(reverse=True)
+    print(freq_list) ### del
+
+    top_freq_list = freq_list[:top_n]
+    print(top_freq_list) ### del
+
+    final_top_freq_list = []
+    for i, element in enumerate(top_freq_list):
+        final_top_freq_list.append(element[1])
+
+    final_top_freq_list = tuple(final_top_freq_list)
+    print(final_top_freq_list) ### del
+
+    return final_top_freq_list
+
+    # добавить защиту, если текст некорректен/пуст
+    # добавить чтение из файла
+
+freq_dict = {}
+freq_list = []
