a8a62ed95fdfc95efc1c1f32fb95c184b55ddd0b
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-09-25 02:09:45 +0300

    Update main.py
    a

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,16 +8,14 @@ Count frequencies dictionary by the given arbitrary text
 
 def calculate_frequences(texts: str) -> dict:
 
-    freq_dictionary = {}
-
-
+    freq_dict = {}
     #   texts_list = []
     if not texts:
         return {}
     if isinstance(texts, str):
         texts = texts.lower()
         texts = texts.split(' ')
-        texts.remove('\n', ' ')
+        texts.replace('\n', ' ')
         while '\n' in texts:
             texts.remove('\n')
         while texts.find(' ') != -1:
@@ -36,40 +34,40 @@ def calculate_frequences(texts: str) -> dict:
                 if part not in errors:
                     new_slovo += part
             if new_slovo != '':
-                if new_slovo not in freq_dictionary:
-                    freq_dictionary[new_slovo] = count_bykva
+                if new_slovo not in freq_dict:
+                    freq_dict[new_slovo] = count_bykva
                 else:
-                    new_count_bykva = freq_dictionary[new_slovo] + 1
-                    freq_dictionary[new_slovo] = new_count_bykva
+                    new_count_bykva = freq_dict[new_slovo] + 1
+                    freq_dict[new_slovo] = new_count_bykva
         #    count_bykva = texts_list.count(bykva)
 
     return  freq_dictionary
 
 def filter_stop_words(freq_dictionary: dict, STOP_WORDS: tuple) -> dict:
 
-    freq_dictionary_new = freq_dictionary.copy()
+    freq_dict_new = freq_dict.copy()
 
-    if not  freq_dictionary:
-        return  freq_dictionary
+    if not  freq_dict:
+        return  freq_dict
     if not STOP_WORDS :
-        return  freq_dictionary
+        return  freq_dict
 
-    for new_stop_word in freq_dictionary.keys():
+    for new_stop_word in freq_dict.keys():
         if not isinstance(new_stop_word, str):
-            freq_dictionary_new.pop(new_stop_word)
+            freq_dict_new.pop(new_stop_word)
     for slovo_stop in STOP_WORDS:
-        if slovo_stop in freq_dictionary_new:
-            freq_dictionary_new.pop(slovo_stop)
-    return freq_dictionary_new
+        if slovo_stop in freq_dict_new:
+            freq_dict_new.pop(slovo_stop)
+    return freq_dict_new
 
 
-def get_top_n(freq_dictionary_new: dict, top_n: int) -> tuple:
+def get_top_n(freq_dict_new: dict, top_n: int) -> tuple:
 
     top_my_list = []
     if top_n <= 0:
         return()
     else:
-        for slovo_stop in freq_dictionary.keys():
+        for slovo_stop in freq_dict.keys():
             top_my_list.append(slovo_stop)
     tuple_top_n = tuple(top_my_list[:top_n])
     return (tuple_top_n)
