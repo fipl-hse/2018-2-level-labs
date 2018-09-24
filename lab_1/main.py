5511d69ac492e9278ce9f554ee09138cfd9823f1
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-09-24 18:33:13 +0300

    Update main.py
    change 1 part

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,21 +3,28 @@ Labour work #1
 
 Count frequencies dictionary by the given arbitrary text
 """
-
-
 def calculate_frequences(texts: str) -> dict:
     my_first_dict = {}
-   # texts = ''
-    if texts == '' or texts is None:
+    bykva = ''
+    texts_list = []
+
+    if texts == '' or texts is None or type(texts) != str:
+        return {}
+    if type(texts) == str:
         texts = texts.lower()
+
         for bykva in texts:
-            if bykva not in 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфчцчшщъыьэюя':
+            if bykva not in 'abcdefghijklmnopqrstuvwxyz':
                 texts = texts.replace(bykva, '')
+            if texts.isdigit():
+                continue
+            if bykva in 'abcdefghijklmnopqrstuvwxyz':
+                texts_list.append(bykva)
+
         texts = texts.split('')
-        if type(texts) == str:
-            for slovo in texts:
-                count_slovo = texts.count(slovo)
-                my_first_dict[slovo] = count_slovo
+
+        for bykva in texts_list:
+            my_first_dict[bykva] = texts_list.count(bykva)
             return my_first_dict
         else:
             return {}
@@ -28,7 +35,6 @@ def filter_stop_words(my_first_dict: dict, stop_words: spisok) -> dict:
         return my_first_dict
 
     my_second_dict = my_first_dict.copy()
-    pass
 
 def get_top_n() -> tuple:
     """
