85863aedc50715bdbb480c0fe907123d19094c7e
KsenyaSH <43419770+KsenyaSH@users.noreply.github.com>
2018-09-24 23:02:31 +0300

    Func 2.0

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,44 +8,56 @@ def print_dictionary(d: dict):
         print(b, d)
 
 def calculate_frequences(text: str) -> dict:
+    frequencies = {}
+
+    if type(text) == str:
+        for i in text:
+            if i.isalpha():
+                pass
+            else:
+                text = text.replace(i, ' ')
+        text = text.lower()
+        words = text.split(' ')
+        for w in words:
+            if w.isalpha():
+                if w in frequencies.keys():
+                    n = frequencies[w]
+                    frequencies[w] = n + 1
+                else:
+                    frequencies[w] = 1
+
+        return frequencies
+    elif text == None or type(text) != str:
+        return frequencies
     """
     Calculates number of times each word appears in the text
     """
-    text = text.lower()
-    words = text.split(' ')
-    frequencies = {}
-    for w in words:
-        if w in frequencies.keys():
-            n = frequencies[w]
-            frequencies[w] = n+1
-        else:
-            frequencies[w] = 1
-
-    return frequencies
+    
 
 frequencies = calculate_frequences(text)
 print('Частотный словарь')
 print_dictionary(frequencies)
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+    frequencies1 = {}
+    if type(stop_words) == tuple:
+        dct1 = dict(frequencies)
+        for k, v in dct1.items():
+            if k in stop_words:
+                del frequencies[k]
+        return frequencies
+    elif stop_words == None or stop_words !=tuple:
+        return frequencies1
     """
     Removes all stop words from the given frequencies dictionary
-    """
-    dct1 = dict(frequencies)
-    for k, v in dct1.items():
-        if k in stop_words:
-            del frequencies[k]
-    return dct1
+    """ 
 
 clear_dct = filter_stop_words(frequencies, stop_words)
 print('Частотный словарь чистый')
 print_dictionary(clear_dct)
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    """
-   res = []
+    res = []
     l = list(frequencies.values())
     l.sort()
     l.reverse()
@@ -62,6 +74,10 @@ def get_top_n(frequencies: dict, top_n: int) -> tuple:
     res = tuple(res)
     print(res)
     return res
+    """
+    Takes first N popular words
+    """
+   
 
 print('Первые', n, 'по популярности слов')
 get_top_n(frequencies, n)
