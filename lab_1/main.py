e7d30b5bbfe9aafb1073b9c371ae278c3fe36c35
alinaalborova <43433331+alinaalborova@users.noreply.github.com>
2018-09-25 07:49:46 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -32,7 +32,6 @@ def calculate_frequences(text: str) -> dict:
 
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    new_frequencies = {}
     for word in frequencies.keys():
         if word not in stop_words:
             new_frequencies[word] = frequencies.get(word)
