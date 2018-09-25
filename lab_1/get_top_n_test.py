3eea8086d7aba55499dc108d2666c4f04a2784b8
captaintb <43381620+captaintb@users.noreply.github.com>
2018-09-25 10:09:43 +0300

    delete test_get_top_n_equal_frequency

--- lab_1/get_top_n_test.py
+++ lab_1/get_top_n_test.py
@@ -65,15 +65,6 @@ class GetTopNTest(unittest.TestCase):
         top_words = main.get_top_n(filtered_dict, 3)
         self.assertEqual(expected_result, top_words)
 
-    def test_get_top_n_equal_frequency(self):
-        """
-        Check that we get top n words ideal case
-        """
-        expected_result = ('fox', 'dog', 'cat')
-        filtered_dict = {'fox': 1, 'dog': 1, 'cat': 1,}
-        top_words = main.get_top_n(filtered_dict, 3)
-        self.assertEqual(expected_result, top_words)
-
     def test_get_top_n_equal_order(self):
         """
         Check that we get top n words ideal case
