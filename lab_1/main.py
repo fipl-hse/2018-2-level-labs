99567f73a409a0270cb91caf869c1d97dde37723
Irina <43418144+ZarIra@users.noreply.github.com>
2018-09-25 08:36:59 +0300

    Laboratory_linting_defsort.py
    Linting changed + def sort created

--- lab_1/main.py
+++ lab_1/main.py
@@ -7,7 +7,7 @@ def calculate_frequences(text) -> dict:
     import re
     if text and isinstance(text, str):
         prepared_text = re.findall(r'[a-z]+', text.lower())
-        for index, element in enumerate(prepared_text):
+        for element in enumerate(prepared_text):
             new_word = ''
             count = 1
             for el_index in range(len(element)): # вводим дополнительный цикл, где избавляемся от лишнего "мусора"
@@ -19,15 +19,16 @@ def calculate_frequences(text) -> dict:
                 new_count = frequent_dict[new_word] + 1
                 frequent_dict[new_word] = new_count
     return frequent_dict
+
 def filter_stop_words(frequent_dict, stop_words) -> dict: # Removes all stop words from the given frequencies dictionary
     new_frequent_dict = {}
     if frequent_dict and stop_words:
         for word, frequency in frequent_dict.items():
             if word not in stop_words and isinstance(word, str):
-                 new_frequent_dict[word] = frequency
+                new_frequent_dict[word] = frequency
         return new_frequent_dict
-    else:
-        return frequent_dict
+    return frequent_dict
+
 def get_top_n(frequent_dict, top_n) -> tuple:
     words = []
     frequencies = []
@@ -36,21 +37,26 @@ def get_top_n(frequent_dict, top_n) -> tuple:
         for word, frequency in frequent_dict.items():
             words.append(word)
             frequencies.append(frequency)
-        for j in range(len(frequencies) - 2): # сортировка строк методом пузырька
-            for i in range(len(frequencies) - j -1):
-                repository_fr = 0
-                repository_words = ''
-                if frequencies[i] < frequencies[i+1]:
-                    repository_fr = frequencies[i]
-                    frequencies[i] = frequencies[i + 1]
-                    frequencies[i + 1] = repository_fr
-                    repository_words = words[i]
-                    words[i] = words[i + 1]
-                    words[i + 1] = repository_words
+        words = sort(frequencies, words)
         for i in range(top_n):
             if i <= (len(words) - 1):
                 tuple_top_n = tuple_top_n[:] + (words[i],)
     return tuple_top_n
+
+def sort(frequencies, words):
+    for j in range(len(frequencies) - 2):  # сортировка строк методом пузырька
+        for i in range(len(frequencies) - j - 1):
+            repository_fr = 0
+            repository_words = ''
+            if frequencies[i] < frequencies[i + 1]:
+                repository_fr = frequencies[i]
+                frequencies[i] = frequencies[i + 1]
+                frequencies[i + 1] = repository_fr
+                repository_words = words[i]
+                words[i] = words[i + 1]
+                words[i + 1] = repository_words
+    return words
+
 TEXT = '''The quick brown fox jumps over the lazy dog'''
 STOPWORDS = ['the', 'lazy']
 N = 2
