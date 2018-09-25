fc73a6044242809915795a307e9442a690f26e2b
Thoola <creizu34@gmail.com>
2018-09-25 06:26:39 +0300

    Final v.

--- lab_1/main.py
+++ lab_1/main.py
@@ -36,15 +36,30 @@ def calculate_frequences() -> dict:
 
 def filter_stop_words() -> dict:
     global filtered_words
+    
+    #filtered_words = {y: freq[y] for y in freq if not (y.isdigit()
+                                            #or freq[0] == '-' and freq[1:].isdigit())}
     filtered_words = {k: freq[k] for k in freq if k not in stpw}
     return filtered_words
     pass
 
 
 def get_top_n() -> tuple:
+    global fin
     top = int(input("Enter the number for top n: "))
     fin = tuple(Counter(filtered_words).most_common(top))
     for i in fin:
         print(i)
     return fin
     pass
+def file_output():
+    final = list(fin)
+    file = str(input('Enter the name of the file (with .txt): '))
+    out = open(file, 'w')
+    for z in final:
+        out.write(str(z))
+        out.write('\n')
+        print (z)
+    out.close()
+    pass
+    
