b228bb33508ff1fa0c3d6bf853df527ea7eb0d34
NatalieAntoshka <eskadron_natalie@mail.ru>
2018-09-25 10:59:31 +0300

    added functions

--- lab_1/main.py
+++ lab_1/main.py
@@ -7,37 +7,58 @@ def calculate_frequences(text):
     text = text.lower()
 
     for i in text:
-        if i in mess and i in text:
+        if i in mess:
             text = text.replace(i, '')
     text = text.split()
     for i in text:
-        num = frequency[i]
-        frequency[i] = num + 1
+        num = text.count(i)
+        frequency[i] = num
     return frequency
 
 
 def filter_stop_words(frequency2,stop_words):
+    if frequency2 == None or stop_words == None:
+        return frequency2
     for i in list(frequency2):
-        if i.isdigit():
+        if str(i).isdigit():
             frequency2.pop(i)
         if i in stop_words:
             frequency2.pop(i)
     return frequency2
 
 
-def get_top_n(frequences,top_n):
-    if not top_n > 0:
-        return ()
-    dic = sorted(frequences.items(), key = lambda i:i[1], reverse=True)
-    dic1=()
-    for i in range(0,top_n):
-        dic1 = dic1 + dic[i]
-    if top_n == 0:
-        dic1 = dic
-    return dic1
+def get_top_n(frequencies, top_n):
+    #new = []
+    #for key,value in frequencies.items():
+        #new.append([value, key])
+    #sorted(new.items(), key=lambda i: i[1], reverse=True)
+    #sorted(new, reverse=True)
+    #dic1 = []
+    #counter = 0
+    #for element in new:
+        #if counter == top_n:
+            #break
+        #dic1 = dic1.append(element[1])
+        #counter += 1
+    #res = tuple(dic1)
+    #return res
+    res = []
+    counter = 0
+
+    for key in frequencies.keys():
+        if counter == top_n:
+            break
+        res.append(key)
+        counter +=1
+    return tuple(res)
 
 
-print('check that')
+
+
+
+
+
+
 # f = calculate_frequences(text)
 # frequency2 = f.copy()
 # print('calculate_frequencies',f)
