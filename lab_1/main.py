5b7f1606c1a1bda812335f7ea0f6632c7efa1ef0
barganserafima <43433209+barganserafima@users.noreply.github.com>
2018-09-25 10:16:21 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,18 +5,23 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 def calculate_frequences(text):
-    text = text.lower()
-    alphabet = 'qazwsxedcrfvtgbyhnujmikolp'
-    clean_text = ''
-    i = 0
-    for i in range(len(text)):
-        if text[i] not in alphabet:
-            clean_text = text.replace(text[i], ' ')
-            text = clean_text
-    splitted_text = text.split(' ')
-    frequencies = {word: splitted_text.count(word) for word in splitted_text}
-    frequencies.pop('')
-    return frequencies
+    if text != None and type(text) != int:
+        text = text.lower()
+        alphabet = 'qazwsxedcrfvtgbyhnujmikolp'
+        clean_text = ''
+        i = 0
+        for i in range(len(text)):
+            if text[i] not in alphabet:
+                clean_text = text.replace(text[i], ' ')
+                text = clean_text
+        splitted_text = text.split(' ')
+        frequencies = {word: splitted_text.count(word) for word in splitted_text}
+        frequencies.pop('')
+        return frequencies
+    else:
+        frequencies = {}
+        return frequencies
+
 
 def filter_stop_words(frequencies, stop_words):
     filtered_dictionary = {}
