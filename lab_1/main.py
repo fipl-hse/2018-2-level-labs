58d95e528b6cb4050b96ff27c9b9eb662b8e9642
alenapeskova <43438782+alenapeskova@users.noreply.github.com>
2018-09-25 09:37:36 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,23 +9,23 @@ def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    d = {}
-    if type(text) is str:
+    dict = {}
+    if isinstance(text,str):
 
         new_text = text.lower()
         punctuation = "~$%&^@*#1234567890\"{}[]\'/\n:;!?().,<>"
 
-        for c in punctuation:
-            new_text = new_text.replace(c,"")
+        for symb in punctuation:
+            new_text = new_text.replace(symb, "")
 
         new_text1 = new_text.split()
 
         for i in range (0,len(new_text1)):
-            d[new_text1[i]] = new_text1.count(new_text1[i])
+            dict[new_text1[i]] = new_text1.count(new_text1[i])
 
-    return d
+    return dict
 
-    pass
+
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
@@ -34,13 +34,13 @@ def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     sorted_words = {}
 
     if not stop_words:
-        stop_words=tuple()
+        stop_words = tuple()
     if frequencies:
         sorted_words = {k: frequencies[k] for k in frequencies if (k not in stop_words) and (type(k) is str)}
 
     return sorted_words
 
-    pass
+
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
     """
@@ -52,5 +52,3 @@ def get_top_n(frequencies: dict, top_n: int) -> tuple:
         top_n = len(sorted_list)
     top_n_words = tuple(sorted_list[i][0] for i in range (top_n))
     return top_n_words
-
-    pass
