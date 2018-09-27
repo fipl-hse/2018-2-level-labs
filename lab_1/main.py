b2237694f5a1f899de8f2788bc154e014b181814
Thoola <creizu34@gmail.com>
2018-09-27 19:21:33 +0300

    

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,35 +1,45 @@
-import re
 from string import punctuation
 from collections import Counter
-r = re.compile(r'[\s]+'.format(re.escape(punctuation)))
-text = '''He do subjects prepared 34 bachelor juvenile ye oh.
-He feelings removing informed he as 34 ignorant we prepared.
-Celebrated if remarkably especially an.
-Going eat set she books found met aware.'''
+text = '''He do subjects *prepared 34 bachelor juvenile ye oh.
+He feelings rem'oving informed he as 34 ignorant we prepared.
+Celebrated if remarkably especia"lly an.
+Goi(ng eat set she books found met aware.'''
 stop_words = ('a', 'an', 'is', 'are', 'am', 'the', 'of',
 'with', 'at', 'to', 'in', 'as')
 top_n = 3
+import collections
+c = collections.Counter()
 def calculate_frequences(text: str) -> dict:
     if text is None:
         text = ''
     words = str(text).lower()
-    words = r.split(words)
+    restr_chars = "~$%&^@*#\"{}[]\'/\n:;!?().,<>"
+    for i in restr_chars:
+        if i in words:
+            if i == '.':
+                words = words.replace(i, ' ')
+            else:
+                words = words.replace(i, '')
+    for k in words:
+        if k.isdigit() or k == '-' and k[1:].isdigit():
+            words = words.replace(k, '')
     global frequencies
-    frequencies = Counter(words).most_common()
-    frequencies = dict(frequencies)
-    frequencies = {k: frequencies[k] for k in frequencies if not k.isdigit() or k == '-' and k[1:].isdigit()}
+    frequencies = words.split()
+    for word in frequencies:
+        c[word] += 1
+    frequencies = c
     return frequencies
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     if stop_words is None:
         stop_words = tuple()
-        
     filtered_w = {k: frequencies[k] for k in frequencies if (k not in stop_words) and (type(k) is str)}
     return filtered_w
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
     if top_n is None or top_n < 0 or type(top_n) == str:
         top_n = 0
-    top_w = frequencies.items()
+    top_w = sorted(frequencies, key=frequencies.get, reverse = True)
     top_w = ((tuple(top_w))[0:top_n])
+    print (top_w)
     return top_w
