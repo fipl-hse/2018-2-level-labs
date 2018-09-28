bee42d8066560c62af64907c9302151103be1201
Thoola <creizu34@gmail.com>
2018-09-28 03:02:16 +0300

    LINT #6

--- lab_1/main.py
+++ lab_1/main.py
@@ -7,12 +7,10 @@ Goi(ng eat set she books found met aware.'''
 STOP_WORDS = ('a', 'an', 'is', 'are', 'am', 'the', 'of',
 'with', 'at', 'to', 'in', 'as')
 TOP_N = 3
-FREQUENCIES = {}
 def calculate_frequences(text: str) -> dict:
     if text is None:
         text = ''
-    global FREQUENCIES
-    frequencies = collections.Counter()
+    c = collections.Counter()
     words = str(text).lower()
     restr_chars = "~$%&^@*#\"{}[]\'/\n:;!?().,<>"
     for i in restr_chars:
@@ -26,21 +24,23 @@ def calculate_frequences(text: str) -> dict:
             words = words.replace(k, '')
     words = words.split()
     for word in words:
-        frequencies[word] += 1
-    frequencies = dict(frequencies)
-    return frequencies
+        c[word] += 1
+    global FREQUENCIES
+    FREQUENCIES = dict(c)
+    return FREQUENCIES
 
 def filter_stop_words(FREQUENCIES: dict, stop_words: tuple) -> dict:
-    if frequencies is None:
-        frequencies = {}
+    if FREQUENCIES is None:
+        FREQUENCIES = {}
     if stop_words is None:
         stop_words = tuple()
-    filtered_w = {k: frequencies[k] for k in frequencies if (k not in stop_words) and (isinstance(k, str))}
+    filtered_w = {k: FREQUENCIES[k] for k in FREQUENCIES if (k not in STOP_WORDS) and (isinstance(k, str))}
     return filtered_w
 
 def get_top_n(FREQUENCIES: dict, top_n: int) -> tuple:
     if top_n is None or top_n < 0 or not isinstance(top_n, int):
         top_n =0 
-    top_w = sorted(frequencies, key=frequencies.get, reverse=True)
+    top_w = sorted(FREQUENCIES, key=FREQUENCIES.get, reverse=True)
     top_w = ((tuple(top_w))[0:top_n])
     return top_w
+
