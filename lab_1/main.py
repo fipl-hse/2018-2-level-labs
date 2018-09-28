1d1c37b1b898d2c25474bd73f82a88b2041b251a
polinaivl <43419013+polinaivl@users.noreply.github.com>
2018-09-28 17:38:41 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -34,7 +34,7 @@ def calculate_frequences(text) -> dict:
     else:
         for i in clean_text:
             c = []
-            c[i] = clean_text.count([i])
+            c[] = clean_text.count([i])
             dict_freq = dict(zip(clean_text, c))
         
     
