3ba7f26e31959b3b78d6b2b583f65d0028ef0d1e
KateAntsiferova <43483202+KateAntsiferova@users.noreply.github.com>
2018-09-30 17:42:30 +0300

    Update main.py
    add some features of function bodies

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,57 +3,42 @@ Labour work #1
 
 Count frequencies dictionary by the given arbitrary text
 """
-frequency = {}
+"""
+Labour work #1
+
+Count frequencies dictionary by the given arbitrary text
+"""
 
 
-def calculate_frequences(base):
+def calculate_frequences(text):
     frequency_dict = {}
-    if type(base) is str:
-        base1 = base.lower()
+    if type(text) is str:
+        text1 = text.lower()
         signs = """?!%~<>/\.,[]{}()@"'#*&0123456789"""
         for c in signs:
-            base1 = base1.replase(c," ")
-            
-        clean_base = base1.lower()
-         
-        for i in range(0, len(clean_base)):
-            frequency_dict[clean_base[i]] = base1.count(clean_base[i])
+            text1 = text1.replase(c, " ")
 
+        clean_text = text1.split()
+
+        for i in range(0, len(clean_text)):
+            frequency_dict[clean_text[i]] =clean_text.count(clean_text[i])
     return frequency_dict
 
 
-
 def filter_stop_words(new_dict, stop_words):
-    
     filtered_words = {}
-    
     if not stop_words:
         stop_words = tuple()
     if new_dict:
-        filtered_words = {key: new_dict[key] for key in new_dict if (key not in wrong_words) and (type(key)  is str)}
-        
+        filtered_words = {key: new_dict[key] for key in new_dict if (key not in stop_words) and (type(key) is str)}
     return filtered_words
-        
-  
+
 
 
 
 def get_top_n(right_words, top_n: int):
-    final = ''
-    for i in range(top_n):
-        final += right_words[i]
-        final += '\n'
-    return final
-
-
-text = '''I always have lunch at 7:00 evenings.
-          My friends and I usually have fun together.
-          365 members of my family are very close to me,
-          my father is so smart.'''
-text = text.lower()
-wrong_words = ('usually', 'have', 'my')
-first_dict = calculate_frequences(text)
-filtered = filter_stop_words(first_dict, wrong_words)
-#N = int(input('enter an appropriate n, please'))
-N = 2
-finish = get_top_n(filtered, N)
+    filtered_list = sorted(right_words.items(), key=lambda item: item[i], reverse=True)
+    if top_n > len(filtered_list):
+        top_n = len(filtered_list)
+    final_words = tuple(filtered_list[i][0] for i in range (top_n))
+    return final_words
