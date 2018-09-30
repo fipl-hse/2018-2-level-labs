9ad002a541def62ada849ba5811443ec7282dde7
Ezerskaia <43433179+Ezerskaia@users.noreply.github.com>
2018-09-30 15:36:59 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,8 +4,8 @@ Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
 
-
-def calculate_frequences(text):
+from collections import Counter
+def calculate_frequences(text:str)-> dict 
     text = text.lower()
     t = []
     punctuation_marks = """['.',','!',','?',':',';','\','/','"','*',
@@ -23,42 +23,28 @@ def calculate_frequences(text):
         else: 
             v[i] = 1
     
-    print(v)
+  
     return v
 
-def filter_stop_words(v,stop_words):
+def filter_stop_words(v:dict,stop_words:tuple)-> dict
     del_list = []
     for key in v.keys():
         if key in stop_words:
             del_list.append(key)
-    print(v)
     
     for element in del_list:
         v.pop(element)
-    print(v)
+ 
+    return v
 
 def get_top_n(v,n):
     v_sort = Counter(v).most_common()
+    print('v_s = ', v_sort)
     top_n = v_sort[:n]
     print('TOP_N = ',top_n)
-    
-text = input('Введите текст: ')
-v = count_words(text)
-stop_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about',
-              'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be',
-              'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself',
-              'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
-              'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through',
-              'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should',
-              'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all',
-              'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in',
-              'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
-              'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has',
-              'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few',
-              'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing',
-              'it', 'how', 'further', 'was', 'here', 'than']
-              
-filter_words(v,stop_words)
-n = int(input('Введите n = ')) # есои n не число
+    temp_list = []
+    for temp in top_n:
+        temp_list.append(temp[0])
 
-get_top_n(v,n)
+    return(tuple(temp_list))
+
