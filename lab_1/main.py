c29a96bec8f8b0c70e709a5cfc562537c93b5189
Ezerskaia <43433179+Ezerskaia@users.noreply.github.com>
2018-10-01 20:49:22 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,22 +9,22 @@ def calculate_frequences(text: str)-> dict:
     vocabulary = {}
     if type(text) == str:
         text = text.lower()
-        t = []
+        text_list = []
         marks_and_numbers = """['0','1','2','3','4','5','6','7','8','9','.',','!',','?',
         ':',';','\','/','"','*','-','_',''','"','@','#','$','%','^','&','(',')','+','=',
         '[',']','{','}','~','<','>','№' ]"""
         for m_n in marks_and_numbers:
             if m_n in text:
                 text = text.replace(m_n, ' ')
-        t = text.split()
-        number = len(t)
-        for i in t:
+        text_list = text.split()
+        number = len(text_list)
+        for i in text_list:
             if vocabulary.get(i):
-                vocabulary[i] += 1     
-            else: 
+                vocabulary[i] += 1
+            else:
                 vocabulary[i] = 1
     elif text == None or type(text) != str:
-        return vocabulary         
+        return vocabulary
     return vocabulary
 
 
@@ -39,19 +39,16 @@ def filter_stop_words(vocabulary: dict, stop_words: tuple)-> dict:
             del_list.append(key)
         if not(isinstance(key, str)):
             del_list.append(key)
-    
     for element in del_list:
         vocabulary.pop(element)
- 
     return vocabulary
     
-
-def get_top_n(vocabulary: dict, n: int)-> tuple:
-    if n < 0:
+def get_top_n(vocabulary: dict, n_top_words: int)-> tuple:
+    if n_top_words < 0:
         return ()
     v_sort = Counter(vocabulary).most_common()
     print('v_s = ', v_sort)
-    top_n = v_sort[:n]
+    top_n = v_sort[:n_top_words]
     print('TOP_N = ', top_n)
     temp_list = []
     for temp in top_n:
