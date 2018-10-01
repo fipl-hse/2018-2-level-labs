d0d47308620c752586dc43e0694f989607589aac
KateAntsiferova <43483202+KateAntsiferova@users.noreply.github.com>
2018-10-01 22:03:31 +0300

    Update main.py
    add some fixed steps

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,7 +8,7 @@ Count frequencies dictionary by the given arbitrary text
 
 def calculate_frequences(text):
     frequency_dict = {}
-    if type(text) is str:
+    if insinstance(text) is str:
         text1 = text.lower()
         signs = """^;:$`?№!%~<>/\.,[]{}()@"'#*&0123456789"""
         for c in signs:
@@ -17,7 +17,7 @@ def calculate_frequences(text):
         clean_text = text1.split()
 
         for i in range(0, len(clean_text)):
-            frequency_dict[clean_text[i]] =clean_text.count(clean_text[i])
+            frequency_dict[clean_text[i]] = clean_text.count(clean_text[i])
     return frequency_dict
 
 
@@ -36,5 +36,5 @@ def get_top_n(new_dict, top_n: int):
     filtered_list = sorted(new_dict.items(), key=lambda item: item[1], reverse=True)
     if top_n > len(filtered_list):
         top_n = len(filtered_list)
-    final_words = tuple(filtered_list[i][0] for i in range (top_n))
+    final_words = tuple(filtered_list[i][0] for i in range(top_n))
     return final_words
