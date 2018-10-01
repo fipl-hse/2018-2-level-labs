bf12d5e74c746e6694cdb9030b2727645c31b364
Ezerskaia <43433179+Ezerskaia@users.noreply.github.com>
2018-10-01 20:10:58 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,7 +5,7 @@ Count frequencies dictionary by the given arbitrary text
 """
 
 from collections import Counter
-def calculate_frequences(text:str)-> dict:
+def calculate_frequences(text: str)-> dict:
     v = {}
     if type(text) == str:
         text = text.lower()
@@ -15,12 +15,12 @@ def calculate_frequences(text:str)-> dict:
         '[',']','{','}','~','<','>','№' ]"""
         for m_n in marks_and_numbers:
             if m_n in text:
-                text = text.replace(m_n,' ')
+                text = text.replace(m_n, ' ')
         t = text.split()
         number = len(t)
         for i in t:
             if v.get(i):
-                v[i] += 1      
+                v[i] += 1     
             else: 
                 v[i] = 1
     elif text == None or type(text) != str:
@@ -28,16 +28,16 @@ def calculate_frequences(text:str)-> dict:
     return v
 
 
-def filter_stop_words(v:dict,stop_words:tuple)-> dict:
-    if not(isinstance(v,dict)):
+def filter_stop_words(v: dict, stop_words: tuple)-> dict:
+    if not(isinstance(v, dict)):
         return {}
-    if not(isinstance(stop_words,tuple)):
+    if not(isinstance(stop_words, tuple)):
         return v
     del_list = []
     for key in v.keys():
         if key in stop_words:
             del_list.append(key)
-        if not(isinstance(key,str)):
+        if not(isinstance(key, str)):
             del_list.append(key)
     
     for element in del_list:
@@ -46,13 +46,13 @@ def filter_stop_words(v:dict,stop_words:tuple)-> dict:
     return v
     
 
-def get_top_n(v:dict,n:int)-> tuple:
+def get_top_n(v: dict, n: int)-> tuple:
     if n < 0:
         return ()
     v_sort = Counter(v).most_common()
     print('v_s = ', v_sort)
     top_n = v_sort[:n]
-    print('TOP_N = ',top_n)
+    print('TOP_N = ', top_n)
     temp_list = []
     for temp in top_n:
         temp_list.append(temp[0])
