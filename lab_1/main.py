4226d971661c8725e36baadfb334d177c1124b87
Ezerskaia <43433179+Ezerskaia@users.noreply.github.com>
2018-10-01 20:23:18 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,7 +6,7 @@ Count frequencies dictionary by the given arbitrary text
 
 from collections import Counter
 def calculate_frequences(text: str)-> dict:
-    v = {}
+    vocabulary = {}
     if type(text) == str:
         text = text.lower()
         t = []
@@ -19,37 +19,37 @@ def calculate_frequences(text: str)-> dict:
         t = text.split()
         number = len(t)
         for i in t:
-            if v.get(i):
-                v[i] += 1     
+            if vocabulary.get(i):
+                vocabulary[i] += 1     
             else: 
-                v[i] = 1
+                vocabulary[i] = 1
     elif text == None or type(text) != str:
-        return v         
-    return v
+        return vocabulary         
+    return vocabulary
 
 
-def filter_stop_words(v: dict, stop_words: tuple)-> dict:
-    if not(isinstance(v, dict)):
+def filter_stop_words(vocabulary: dict, stop_words: tuple)-> dict:
+    if not(isinstance(vocabulary, dict)):
         return {}
     if not(isinstance(stop_words, tuple)):
-        return v
+        return vocabulary
     del_list = []
-    for key in v.keys():
+    for key in vocabulary.keys():
         if key in stop_words:
             del_list.append(key)
         if not(isinstance(key, str)):
             del_list.append(key)
     
     for element in del_list:
-        v.pop(element)
+        vocabulary.pop(element)
  
-    return v
+    return vocabulary
     
 
-def get_top_n(v: dict, n: int)-> tuple:
+def get_top_n(vocabulary: dict, n: int)-> tuple:
     if n < 0:
         return ()
-    v_sort = Counter(v).most_common()
+    v_sort = Counter(vocabulary).most_common()
     print('v_s = ', v_sort)
     top_n = v_sort[:n]
     print('TOP_N = ', top_n)
