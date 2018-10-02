0bdc4f025b15e75fd6a95165aa0cfc1fcc03a4d7
mariia1299 <43433093+mariia1299@users.noreply.github.com>
2018-10-02 18:53:05 +0300

    Update main.py
    added arguments

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,6 +1,13 @@
 
+
 #Labour work #1
-def calculate_frequences(text) -> dict:
+import re
+from collections import Counter
+
+counted_words = {}
+current_word = []
+text = 'aa, bcd, 1234, aa'
+def calculate_frequences(text):
     text = re.sub(r'[^\w\s]+|[\d]+','', text)
     text = text.lower()
     text = text.split()
@@ -9,27 +16,30 @@ def calculate_frequences(text) -> dict:
     for current_word in text:
         value =  text.count(current_word)  # берет эл-т из списка и считает сколько раз этот эл=т встр в этом списке
         counted_words[current_word] = value # на каждом шагу по этому циклу запишет в ключ сколько это слов употребл в тексте
-    return {}
+    return counted_words
+res = calculate_frequences(text)
+print(res)
 
-def filter_stop_words() -> dict:
-   counted_words_new = counted_words.copy()
+def filter_stop_words(counted_words, stop_words):
+    counted_words_new = counted_words.copy()
 
     for current_word in list(counted_words_new):# словарь переводим в список, эл-т в спискее=ключ
         if current_word in stop_words:# current word = key
             counted_words_new.pop(current_word)
-    return counted_words_new
-    pass
+    return (counted_words_new)
 
-def get_top_n() -> tuple:
-   counted_words_new = sorted(counted_words_new.items(), key = lambda i:i[1], reverse=True)
-    counter  = 0
+
+def get_top_n(counted_words_new, top_n) -> tuple:
+    counted_words_new = sorted(counted_words_new.items(), key = lambda i:i[1], reverse=True)
+    counter = 0
     lists = []
+
     for key in counted_words_new:
         if counter == top_n:
             break
-        else: 
+        else:
             lists.append(key[0])
             counter  +=1
             continue
-    return tuple(lists)
-    pass
+    return tuple (lists)
+
