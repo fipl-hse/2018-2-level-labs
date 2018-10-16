02d2fa4c43449256bb63105d9519079cdde27a8a
irenemfzlv <43378074+irenemfzlv@users.noreply.github.com>
2018-10-16 09:09:08 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -11,3 +11,18 @@ if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+     
+def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
+  pass
+ 
+def keep_known(candidates: tuple, frequencies: dict) -> list:
+  pass
+ 
+def choose_best(frequencies: dict, candidates: tuple) -> str:
+  pass
+
+def spell_check_word(frequencies: dict, as_is_words: tuple, word: str) -> str:
+  pass
+ 
+def spell_check_text(frequencies: dict, as_is_words: tuple, text: str) -> str:
+  pass
