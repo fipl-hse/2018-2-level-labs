08518568b864b0faf92a3d85c27cabcce8370aa3
pdpetrova <pdpetrova00@gmail.com>
2018-10-16 16:17:35 +0300

    First function

--- lab_2/main.py
+++ lab_2/main.py
@@ -11,3 +11,21 @@ if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+
+
+def propose_candidates(word, max_depth_permutations: int=1) -> list:
+    variations = []
+    if not word == '' and None:
+        for i in range(len(word) + 1):
+            for a in LETTERS:
+                the_word = word[:i] + a + word[i:]
+                variations.append(the_word)
+        for i in range(len(word)):
+            variations.append(word[:i] + word[i + 1:])
+            for a in LETTERS:
+                new_word = word[:i] + a + word[i + 1:]
+                variations.append(new_word)
+        for i in range(len(word) - 1):
+            variations.append(word[0:i] + word[i + 1] + word[i] + word[i + 2:])
+    final_result = list(set(variations))
+    return final_result
