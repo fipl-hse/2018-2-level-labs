d48f4a91ae0d2bc9f08da1836e7e0e529fa84b19
alinaalborova <43433331+alinaalborova@users.noreply.github.com>
2018-10-16 13:25:17 +0300

    added the 1st function. it's not complete though
    There's no code for max_depth_permutations yet, but it will be added

--- lab_2/main.py
+++ lab_2/main.py
@@ -11,3 +11,40 @@ if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+
+def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
+ 
+ if word != '' and word != None:
+     l_word = word.lower()
+     splits = []
+     for i in range(len(l_word)+1):
+         splits.append([l_word[:i], l_word[i:]])
+
+     deletes = []
+     for left, right in splits:
+         if right != '':
+             deletes.append(left + right[1:])
+
+     interchange = []
+     for i in range(len(l_word)-1):
+         interchange.append(l_word[:i] + l_word[i + 1] + l_word[i] + l_word[i + 2:])
+
+     inserted = []
+     for letter in LETTERS:
+         for i in range(len(l_word) + 1):
+             inserted.append(l_word[:i] + letter + l_word[i:])
+
+     changed_to_new = []
+     for letter in LETTERS:
+         for i in range(len(l_word)):
+             changed_to_new.append(l_word[:i] + letter + l_word[i+1:])
+
+     all_changes = changed_to_new + inserted + interchange + deletes
+     permutations = len(changed_to_new) + len(inserted) + len(interchange) + len(deletes)
+     all_changes_set = set(all_changes)
+     clear_changes = list(all_changes_set)
+
+ else:
+     clear_changes = []
+
+    
