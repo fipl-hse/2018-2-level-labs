db76bced4ca5d23e5ea30b2a57876c44fe128ef6
irenemfzlv <43378074+irenemfzlv@users.noreply.github.com>
2018-10-22 00:51:16 +0500

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -13,7 +13,32 @@ if __name__ == '__main__':
         freq_dict = calculate_frequences(REFERENCE_TEXT)
      
 def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
-  pass
+    candidates = []
+    let = 'abcdefghijklmnopqrstuvwxyz'
+
+    for i in range(len(text)): #delete the character
+        no_letter = text[:i]+text[i+1:]
+        if no_letter not in candidates:
+            candidates.append(no_letter)
+
+    for i in range(len(text)): #add the letter between the characters
+        for el in let:
+            add = text[:i] + el + text[i:]
+            if add not in candidates:
+                candidates.append(add)
+
+    for i in range(len(text)): #replace the character with a letter
+        for el in let:
+            replace = text[:i]+el+text[i+1:]
+            if replace not in candidates:
+                candidates.append(replace)
+
+    for i in range(0, len(text)-1): #swap 2 characters
+        swap = text[:i] + text[i + 1] + text[i] + text[i + 2:]
+        if swap not in candidates:
+            candidates.append(swap)
+
+    return candidates
  
 def keep_known(candidates: tuple, frequencies: dict) -> list:
   pass
