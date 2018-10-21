3c53c096336a0c3de92da20f7a5d8a4d58d7d634
admin <zadvornovk@mail.ru>
2018-10-21 23:31:07 +0300

    first commit in the second work

--- lab_2/main.py
+++ lab_2/main.py
@@ -2,12 +2,26 @@
 Labour work #2
  Check spelling of words in the given  text
 """
-from lab_1.main import calculate_frequences
+#from lab_1.main import calculate_frequences
 
-LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-REFERENCE_TEXT = ''
+#LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+#REFERENCE_TEXT = ''
 
-if __name__ == '__main__':
-    with open('very_big_reference_text.txt', 'r') as f:
-        REFERENCE_TEXT = f.read()
-        freq_dict = calculate_frequences(REFERENCE_TEXT)
+#if __name__ == '__main__':
+    #with open('very_big_reference_text.txt', 'r') as f:
+        #REFERENCE_TEXT = f.read()
+        #freq_dict = calculate_frequences(REFERENCE_TEXT)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+lst = []
+lst1 = []
+lst2 = []
+word = "dog"
+for j in range(0, len(alphabet)):
+    for i in range(0,len(word)):
+        word = list(word)
+        word.insert(i, " ")
+        lst.append(word)
+        for i in range(0,len(lst)):
+            lst[i] = ''.join(lst[i])
+            lst[i] = lst[i].replace(" ", alphabet[j])
+print(lst)
