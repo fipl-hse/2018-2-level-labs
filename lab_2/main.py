9d7fb3d504bcd310a7ebc98b863f2fe3899d6f7a
irenemfzlv <43378074+irenemfzlv@users.noreply.github.com>
2018-10-22 00:56:03 +0500

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -15,26 +15,28 @@ if __name__ == '__main__':
 def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
     candidates = []
     let = 'abcdefghijklmnopqrstuvwxyz'
+    if word == None:
+        return []
 
-    for i in range(len(text)): #delete the character
-        no_letter = text[:i]+text[i+1:]
+    for i in range(len(word)): #delete the character
+        no_letter = word[:i] + word[i + 1:]
         if no_letter not in candidates:
             candidates.append(no_letter)
 
-    for i in range(len(text)): #add the letter between the characters
+    for i in range(len(word)): #add the letter between the characters
         for el in let:
-            add = text[:i] + el + text[i:]
+            add = word[:i] + el + word[i:]
             if add not in candidates:
                 candidates.append(add)
 
-    for i in range(len(text)): #replace the character with a letter
+    for i in range(len(word)): #replace the character with a letter
         for el in let:
-            replace = text[:i]+el+text[i+1:]
+            replace = word[:i] + el + word[i + 1:]
             if replace not in candidates:
                 candidates.append(replace)
 
-    for i in range(0, len(text)-1): #swap 2 characters
-        swap = text[:i] + text[i + 1] + text[i] + text[i + 2:]
+    for i in range(0, len(word)-1): #swap 2 characters
+        swap = word[:i] + word[i + 1] + word[i] + word[i + 2:]
         if swap not in candidates:
             candidates.append(swap)
 
