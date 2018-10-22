2a336a1874d2001fa1b94b46c86465e0c89cea9e
KsenyaSH <43419770+KsenyaSH@users.noreply.github.com>
2018-10-22 17:57:20 +0300

    func2+func3

--- lab_2/main.py
+++ lab_2/main.py
@@ -11,3 +11,38 @@ if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+def keep_known(candidates: tuple, frequencies: dict) -> list:
+    lst = []
+    if type(frequencies)== dict:
+        frequencies1 = {}
+        for k, v in frequencies.items():
+            if type(k) == str:
+                frequencies1[k] = v
+        if type(candidates) == tuple:
+            for i in candidates:
+                if i in frequencies1:
+                    lst.append(i)
+        else:
+            lst = []
+    else:
+        lst = []
+    return lst
+
+
+def choose_best(frequencies: dict, candidates: tuple) -> str:
+    if candidates == () or type(candidates) != tuple or frequencies == {} or type(frequencies) != dict:
+        finalresult = 'UNK'
+
+    else:
+        lst = keep_known(candidates, frequencies)
+
+        frequencies1 = {}
+        for i in lst:
+            for k, v in frequencies.items():
+                if k in lst:
+                    frequencies1[k] = v
+        lst1 = sorted(frequencies1.items(), key=lambda x: x[1])
+        lst1.reverse()
+        finalresult = lst1[0][0]
+
+    return finalresult
