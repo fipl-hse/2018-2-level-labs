dbb1fc12dfa2efab6456b4b6033fdda0c7fa7231
captaintb <43381620+captaintb@users.noreply.github.com>
2018-10-22 16:26:05 +0300

    First update

--- lab_2/main.py
+++ lab_2/main.py
@@ -5,9 +5,12 @@ Labour work #2
 from lab_1.main import calculate_frequences
 
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+LETTERS.SPLIT()
+
 REFERENCE_TEXT = ''
 
 if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+     
