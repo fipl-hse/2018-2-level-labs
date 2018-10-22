52881f304e612a40b609219c83a1024f173629e2
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-10-22 13:39:32 +0300

    Update main.py
    add part1

--- lab_1/main.py
+++ lab_1/main.py
@@ -118,5 +118,4 @@ def propose_candidates(word: str, max_depth_permutations: int = 1) -> list:
     for position in range(len(word) - 1):
         candidates = (word[:position] + word[position + 1] + word[position] + word[position + 2:])
         candidates_list.add(candidates)
-
     return list(candidates_list)
