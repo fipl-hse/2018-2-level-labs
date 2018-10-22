8ed76fc627b53d5f616548cea407a76d03567097
barganserafima <43433209+barganserafima@users.noreply.github.com>
2018-10-22 11:06:00 +0300

    changed 1st

--- lab_2/main.py
+++ lab_2/main.py
@@ -12,32 +12,34 @@ if __name__ == '__main__':
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
      
-def propose_candidates(word, max_depth_permutations):
-    if word != '' and word != None:
-        low_word = word.lower()
-        split_list = []
-        for i in range(len(low_word) + 1):
-            split_list.append([low_word[:i], low_word[i:]])
-        delete_list = []
-        for left_part, right_part in split_list:
-            if right_part != '':
-                delete_list.append(left_part + right_part[1:])
-        inside_change = []
-        for i in range(len(low_word) - 1):
-            inside_change.append(low_word[:i] + low_word[i + 1] + low_word[i] + low_word[i + 2:])
-        pasted = []
-        for symbol in LETTERS:
+def propose_candidates(word, max_depth_permutations: int = 1):
+    if word != '' and word is not None and max_depth_permutations > 0 and type(max_depth_permutations) == int:
+            low_word = word.lower()
+            split_list = []
             for i in range(len(low_word) + 1):
-                pasted.append(low_word[:i] + symbol + low_word[i:])
-        changed = []
-        for symbol in LETTERS:
-            for i in range(len(low_word)):
-                changed.append(low_word[:i] + symbol + low_word[i + 1:])
-        fully_changed = changed + pasted + inside_change + delete_list
-        fully_changed_set = set(fully_changed)
-        result_changes = list(fully_changed_set)
+                split_list.append([low_word[:i], low_word[i:]])
+            delete_list = []
+            for left_part, right_part in split_list:
+                if right_part != '':
+                    delete_list.append(left_part + right_part[1:])
+            inside_change = []
+            for i in range(len(low_word) - 1):
+                inside_change.append(low_word[:i] + low_word[i + 1] + low_word[i] + low_word[i + 2:])
+            pasted = []
+            for symbol in LETTERS:
+                for i in range(len(low_word) + 1):
+                    pasted.append(low_word[:i] + symbol + low_word[i:])
+            changed = []
+            for symbol in LETTERS:
+                for i in range(len(low_word)):
+                    changed.append(low_word[:i] + symbol + low_word[i + 1:])
+            fully_changed = changed + pasted + inside_change + delete_list
+            fully_changed_kit = set(fully_changed)
+            result_changes = list(fully_changed_kit)
+            return result_changes
     else:
         result_changes = []
+        return result_changes
       
 def keep_known(candidates, as_is_words, frequencies):
     all_filtered_words = []
