c96b1bf4858174dcdc0bf619ddb9e3ab9a68dd19
nikakonde <43417233+nikakonde@users.noreply.github.com>
2018-10-22 11:35:35 +0300

    added return

--- lab_2/main.py
+++ lab_2/main.py
@@ -4,32 +4,33 @@ Labour work #2
 """
 from lab_1.main import calculate_frequences
 
-LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
 REFERENCE_TEXT = ''
 
 if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
-     
-    candidate_list = []
+
+candidate_list = []
 alphabet = 'abcdefghijklmnoprstquvwxyz'
+word ='cat'
 
 def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
     if word is None or word == '' or max_depth_permutations != 1:
-        return([])
-    #if word != '' or max_depth_permutations == 1 or word != None:
+       return([])
+
+
     for index in range(1,len(word)):
-        candidate_list.append(word[:index] + word[index + 1:])  # list.append(new_word) #удаление одной буквы из слова
-           #candidate_list.append(word[:index-1] + word[index] + word[index-1] + word[index+1:])
+        candidate_list.append(word[:index] + word[index + 1:])  #удаление одной буквы из слова
         candidate_list.append(word[:index+2]+word[index]+word[index+2:])
 
-            # print(new_word)
 
     for symbol in alphabet:
         for index in range(0, len(word)):
             candidate_list.append(word[:index] + symbol + word[index + 1:])  # замена каждой буквы на букву из алфавита
             candidate_list.append(word[:index] + symbol + word[index:])  # добавление одной буквы в каждое место слова
+
     candidate_list_without_double_words = []
 
     for word in candidate_list:
@@ -38,3 +39,4 @@ def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
 
         else:
              candidate_list_without_double_words.append(word)
+    return(candidate_list_without_double_words)
