771b97cb14f65fcdd5d3960fd4186e082c6748a2
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-10-23 17:11:14 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -8,6 +8,11 @@ from lab_1.main import calculate_frequences
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 REFERENCE_TEXT = ''
 
+ if __name__ == '__main__':
+    with open('very_big_reference_text.txt', 'r') as f:
+        REFERENCE_TEXT = f.read()
+        freq_dict = calculate_frequences(REFERENCE_TEXT)
+
 def propose_candidates(word: str, max_depth_permutations: int = 1) -> str:
     if not word or word == '' or not isinstance(max_depth_permutations, int):
         return []
@@ -90,7 +95,4 @@ def spell_check_word(frequencies: dict, as_is_words: tuple, word: str) -> str:
     itog_word = choose_best(frequencies, tuple(list_known_candidates))
     return itog_word
 
-   if __name__ == '__main__':
-    with open('very_big_reference_text.txt', 'r') as f:
-        REFERENCE_TEXT = f.read()
-        freq_dict = calculate_frequences(REFERENCE_TEXT)
+  
