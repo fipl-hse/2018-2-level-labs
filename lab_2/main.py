0d63f70f23989cecaffdf0bfdaae73744a6b0dcf
polinaivl <43419013+polinaivl@users.noreply.github.com>
2018-10-26 17:04:20 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -107,7 +107,7 @@ def spell_check_word(frequencies: dict, as_is_words: tuple, word: str):
     if word.upper() in as_is_words and type(word) == str:
         result = word
     if as_is_words == ((),):
-        result = 'UNK'
+        result = word
     if type(as_is_words) != tuple:
         candidates = tuple(propose_candidates(word, 1))
         new = tuple(keep_known(candidates, frequences))
