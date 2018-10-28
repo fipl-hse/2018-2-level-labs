249dfad9e52c6bf9e1a9b3e1a4845996700f8390
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-10-28 20:07:49 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -2,39 +2,40 @@
 Labour work #2
  Check spelling of words in the given  text
 """
- from lab_1.main import calculate_frequences
- LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+from lab_1.main import calculate_frequences
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 REFERENCE_TEXT = ''
- if __name__ == '__main__':
+if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
         FREQ_DICT = calculate_frequences(REFERENCE_TEXT)
- def propose_candidates(word: str, max_depth_permutations: int = 1) -> list:
+
+def propose_candidates(word: str, max_depth_permutations: int = 1) -> list:
     if not isinstance(word, str) or word == '' or not isinstance(max_depth_permutations, int):
         return []
     if max_depth_permutations <= 0:
         return[]
-     candidates_list = set()
+    candidates_list = set()
     for position in range(len(word)):
         candidates = (word[:position] + word[position + 1:])
         candidates_list.add(candidates)
-     for symbol in LETTERS:
+        for symbol in LETTERS:
         for position in range(len(word)):
             candidates = (word[:position] + symbol + word[position:])
             candidates_list.add(candidates)
             if position == 0:
                 candidates = symbol + word
                 candidates_list.add(candidates)
-     for symbol in LETTERS:
+    for symbol in LETTERS:
         for position in range(len(word) + 1):
             candidates = (word[:position] + symbol + word[position + 1:])
             candidates_list.add(candidates)
-     for position in range(len(word) - 1):
+    for position in range(len(word) - 1):
         candidates = (word[:position] + word[position + 1] + word[position] + word[position + 2:])
         candidates_list.add(candidates)
-     return list(candidates_list)
- def keep_known(candidates: tuple, frequencies: dict) -> list:
+    return list(candidates_list)
+def keep_known(candidates: tuple, frequencies: dict) -> list:
     if (not isinstance(candidates, tuple)) or (not isinstance(frequencies, dict)):
         return[]
     known_candidates = []
@@ -93,7 +94,6 @@ def choose_best(frequencies: dict, candidates: tuple) -> str:
   #  itog_list.sort()
  #   good_candidate = itog_list[0]
   #  return good_candidate
-
 def spell_check_word(frequencies: dict, as_is_words: tuple, word: str) -> str:
     if (not isinstance(word, str)) or (not isinstance(frequencies, dict)):
         return 'UNK'
