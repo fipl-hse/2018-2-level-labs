42aaf89899831fe61c148960b354f5593ae9eec7
dmitry-uraev <uraev.dima@yandex.ru>
2018-11-20 16:13:22 +0300

    Added split_by_sentence function with all tests done

--- lab_3/main.py
+++ lab_3/main.py
@@ -41,4 +41,41 @@ def encode(storage_instance, corpus) -> list:
 
 
 def split_by_sentence(text: str) -> list:
-    pass
+
+    # Step 0. Test processing.
+    if text is '' or text is None:
+        return []
+    if text[-1] not in ['.', '!', '?']:
+        return []
+
+    # Step 1. Making tokens.
+    alphabet_checker = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    new_text = ''
+    for element in text:
+        if element in ['.', '!', '?']:
+            new_text += '.'
+        if element is ' ':
+            new_text += ' '
+        if element in alphabet_checker:
+            new_text += element
+    final = ''
+    for index in range(len(new_text)-1):
+        if new_text[index] is '.':
+            if new_text[index+1] in alphabet_checker:
+                continue
+            if new_text[index+1] is '.':
+                continue
+        final += new_text[index]
+    final = final.split('.')
+    my_tokens = []
+    for element in final:
+        element = element.lower()
+        element = '<s> ' + element + ' </s>'
+        element = element.split()
+        my_tokens.append(element)
+    return my_tokens
