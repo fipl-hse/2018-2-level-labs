3f8d14c9fb76d4a2f510764e43387a9ffcc36c02
Irina <43418144+ZarIra@users.noreply.github.com>
2018-11-25 19:31:49 +0300

    Three-grams

--- lab_3/main.py
+++ lab_3/main.py
@@ -63,63 +63,70 @@ class NGramTrie:
 
     def fill_from_sentence(self, sentence: tuple) -> str:
         if sentence and isinstance(sentence, tuple):
-            for index in range(len(sentence) - 1):
-                bi_gram = (sentence[index], sentence[index + 1])
-                if bi_gram not in self.gram_frequencies.keys():
-                    self.gram_frequencies[bi_gram] = 1
+            for index in range(len(sentence) - 2):
+                three_gram = (sentence[index], sentence[index + 1], sentence[index + 2])
+                if three_gram not in self.gram_frequencies.keys():
+                    self.gram_frequencies[three_gram] = 1
                 else:
-                    present_value = self.gram_frequencies[bi_gram]
-                    self.gram_frequencies[bi_gram] = present_value + 1
+                    present_value = self.gram_frequencies[three_gram]
+                    self.gram_frequencies[three_gram] = present_value + 1
             return 'OK'
         else:
             return 'ERROR'
 
     def calculate_log_probabilities(self):
         import math
-        for bi_gram in self.gram_frequencies.keys():
-            w = bi_gram[0]
+        for three_gram in self.gram_frequencies.keys():
+            first_word = three_gram[0]
+            second_word = three_gram[1]
             denominator = 0
-            for bi_gram_2 in self.gram_frequencies.keys():
-                if bi_gram_2[0] == w:
-                    denominator += self.gram_frequencies[bi_gram_2]
-            numerator = self.gram_frequencies[bi_gram]
+            for three_gram_2 in self.gram_frequencies.keys():
+                if three_gram_2[0] == first_word and three_gram_2[1] == second_word:
+                    denominator += self.gram_frequencies[three_gram_2]
+            numerator = self.gram_frequencies[three_gram]
             if denominator > 0 and len(self.gram_frequencies.keys()) >= 2:
                 log_probability = math.log(numerator / denominator)
             else:
                 log_probability = 0.0
-            self.gram_log_probabilities[bi_gram] = log_probability
+            self.gram_log_probabilities[three_gram] = log_probability
 
-    def get_bi_gram_by_probability(self, probability: int, dictionary: dict) -> tuple:
+    def get_three_gram_by_probability(self, probability: int, dictionary: dict) -> tuple:
         if probability in dictionary.values():
-            for bi_gram in dictionary.keys():
-                if probability == dictionary[bi_gram]:
-                    return bi_gram
+            for three_gram in dictionary.keys():
+                if probability == dictionary[three_gram]:
+                    return three_gram
         else:
             return 'UNK'
 
     def helper_for_prediction(self, prefix: tuple) -> tuple: #ищет максимальную вероятность из би-граммов вида (<число из префикса>, <что найдется в словаре>)
         current_dict = {}
-        for bi_gram in self.gram_log_probabilities.keys():
-            if bi_gram[0] == prefix[0]:
-                current_dict[bi_gram] = self.gram_log_probabilities[bi_gram]
-        probability_of_most_probable_bi_gram = max(current_dict.values())
-        result = NGramTrie.get_bi_gram_by_probability(self, probability_of_most_probable_bi_gram, current_dict)
+        for three_gram in self.gram_log_probabilities.keys():
+            if three_gram[0] == prefix[0] and three_gram[1] == prefix[1]:
+                current_dict[three_gram] = self.gram_log_probabilities[three_gram]
+        if current_dict:
+            probability_of_most_probable_three_gram = max(current_dict.values())
+        else:
+            probability_of_most_probable_three_gram = 0
+        result = NGramTrie.get_three_gram_by_probability(self, probability_of_most_probable_three_gram, current_dict)
         return result
 
     def predict_next_sentence(self, prefix: tuple) -> list:
         import re
         result = []
         if prefix and isinstance(prefix, tuple):
-            if len(prefix) == 1:
+            if len(prefix) == 2:
                 result += prefix
-                str_of_bi_grams = ''
-                for bi_gram in self.gram_log_probabilities.keys():
-                    str_of_bi_grams += str(bi_gram)
-                    str_of_bi_grams += ' '
-                while re.search(str(prefix), str_of_bi_grams):
+                str_of_three_grams = ''
+                for three_gram in self.gram_log_probabilities.keys():
+                    str_of_three_grams += str(three_gram)
+                    str_of_three_grams += ' '
+                while re.search(str(prefix), str_of_three_grams):
                     next_word = NGramTrie.helper_for_prediction(self, prefix)
-                    result.append(next_word[1])
-                    prefix = (next_word[1],)
+                    if next_word != 'UNK':
+                        result.append(next_word[2])
+                        prefix = (next_word[1], next_word[2],)
+                    else:
+                        break
         return result
 
        
