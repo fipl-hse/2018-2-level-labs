818cabfa7413c4e35e1eda9ce203140f3ba50807
Marat Fatekhov <marat.fatekhov@intel.com>
2018-11-26 11:59:31 +0300

    aligned all except lab_3 with parent repo

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,43 +2,183 @@
 Labour work #3
  Building an own N-gram model
 """
-
+import collections
+import more_itertools
 import math
+import operator
 
-REFERENCE_TEXT = ''
+cnt = collections.Counter()
 if __name__ == '__main__':
     with open('not_so_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
 
+number_sentences = 3
+n = 3
+
+
+def split_by_sentence(REFERENCE_TEXT):
+    if REFERENCE_TEXT is None or not isinstance(REFERENCE_TEXT, str):
+        return []
+    restr_chars = "~$%&^@*#\"{}[]\'/\n:.;!?(),<>"
+    l_text = REFERENCE_TEXT.lower()
+    for i in restr_chars:
+        if i in l_text:
+            if i in '.!?':
+                l_text = l_text.replace(i, '.')
+            else:
+                l_text = l_text.replace(i, '')
+    dots = tuple(more_itertools.locate(l_text, lambda x: x == '.'))
+    for ind in dots:
+        if ind + 1 >= len(l_text) or l_text[ind + 1] == ' ':
+            pass
+        elif l_text[ind + 1] != None:
+            l_text = l_text[:ind] + 'Ê' + l_text[ind + 1:]
+        else:
+            pass
+    l_text = l_text.replace('Ê', '')
+    cut_text = l_text.split('.')
+    for sentence in range(len(cut_text)):
+        cut_text[sentence] = cut_text[sentence].split()
+    for element in cut_text:
+        if len(element) == 0:
+            cut_text.pop(cut_text.index(element))
+    for element in cut_text:
+        if len(element) <= 1:
+            return []
+        else:
+            element.append('</s>')
+            element.insert(0, '<s>')
+    return cut_text
+
 
 class WordStorage:
-    def put(self, word: str) -> int:
-        pass
 
-    def get_id_of(self, word: str) -> int:
-        pass
+    def __init__(self):
+        self.storage = dict()
+        self.counter = 0
 
-    def get_original_by(self, id: int) -> str:
-        pass
+    def put(self, word):
+        if word is None or not isinstance(word, str):
+            return {}
+        if word not in self.storage.values():
+            self.counter = self.counter + 1
+            self.storage.update({word: self.counter})
+            return self.counter
+        else:
+            pass
 
-    def from_corpus(self, corpus: tuple):
-        pass
+    def get_id_of(self, word):
+        if word is None or not isinstance(word, str):
+            return -1
+        for k, v in self.storage.items():
+            if word == k:
+                return v
+        if word not in self.storage.keys():
+            return -1
+
+    def get_original_by(self, number):
+        if number is None or not isinstance(number, int):
+            return 'UNK'
+        for k, v in self.storage.items():
+            if number == v:
+                return k
+        if number not in self.storage.values():
+            return 'UNK'
+
+    def from_corpus(self, sentence):
+        if sentence is None or not isinstance(sentence, tuple):
+            return {}
+        for elements in sentence:
+            ws.put(str(elements))
+
+
+def encode(storage, cut_text):
+    corpus = cut_text
+    for elements in corpus:
+        for syllabus in elements:
+            for k, v in storage.items():
+                if syllabus == k:
+                    elements[elements.index(syllabus)] = v
+    return corpus
 
 
 class NGramTrie:
-    def fill_from_sentence(self, sentence: tuple) -> str:
-        pass
+
+    def __init__(self, n):
+        self.gram_frequencies = dict()
+        self.size = n
+        self.gram_log_probabilities = dict()
+        self.chaotic_grams = []
+        # self.candidates = dict()
+
+    def fill_from_sentence(self, sentence):
+        chaotic_grams = []
+        if sentence is () or not isinstance(sentence, tuple):
+            return {}
+        right = self.size
+        left = 0
+        for tokens in range(len(sentence)):
+            chaotic_grams.append(tuple(sentence[left:right]))
+            right = right + 1
+            left = left + 1
+            if right > len(sentence):
+                break
+        for gram in chaotic_grams:
+            cnt[gram] += 1
+        self.gram_frequencies = dict(cnt)
+
 
     def calculate_log_probabilities(self):
-        pass
+        for k, v in self.gram_frequencies.items():
+            appropriate_values = 0
+            for key, value in self.gram_frequencies.items():
+                if k[:self.size - 1] == key[:self.size - 1]:
+                    appropriate_values = appropriate_values + int(value)
+            if appropriate_values == 0:
+                self.gram_log_probabilities[k] = 0.0
+            else:
+                self.gram_log_probabilities[k] = math.log(v / appropriate_values)
 
-    def predict_next_sentence(self, prefix: tuple) -> list:
-        pass
+    def predict_next_sentence(self, prefix):
+        if prefix is None or not isinstance(prefix, tuple):
+            return []
+        if len(prefix) != self.size - 1:
+            return []
+        best_grams = []
+        candidates = {}
+        for gram, log_prob in self.gram_log_probabilities.items():
+            if gram[0:self.size - 1] == prefix:
+                candidates.update({gram: log_prob})
+        if len(candidates) <= 1:
+            return list(prefix)
+        al_eq = max(candidates.items(), key=operator.itemgetter(1))[0]
+        for k, value in candidates.items():
+            if value >= candidates.get(al_eq):
+                best_grams.append(k)
+        perf_gram = list(sorted(best_grams)[0])
+        return perf_gram
 
 
-def encode(storage_instance, corpus) -> list:
-    pass
+def generate_text(ngr, number_sentences, prefix):
+    best_grams = []
+    for sentence in encode(ws.from_corpus(split_by_sentence(REFERENCE_TEXT)), split_by_sentence(REFERENCE_TEXT)):
+        ngr.fill_from_sentence(sentence)
+    ngr.calculate_log_probabilities()
+    pre_grams = ngr.predict_next_sentence(prefix)
+    for i in range(number_sentences):
+        al_eq = max(pre_grams.items(), key=operator.itemgetter(1))[0]
+        for k, value in pre_grams.items():
+            if value >= pre_grams.get(al_eq):
+                best_grams.append(k)
+        perf_gram = sorted(best_grams)[0]
+        best_grams.pop(best_grams.index(perf_gram))
+        decoded_grams = [ws.get_original_by(i) for i in perf_gram]
+        if best_grams is []:
+            pass
+        return decoded_grams
 
 
-def split_by_sentence(text: str) -> list:
-    pass
+ws = WordStorage()
+ngr = NGramTrie(n)
+
+
