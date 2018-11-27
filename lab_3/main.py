57abdf5cbe5acdd7625de725b0f64d8cbe632634
admin <zadvornovk@mail.ru>
2018-11-27 23:25:01 +0300

    Adaptation of WordStorage and Splitter to tests

--- lab_3/main.py
+++ lab_3/main.py
@@ -12,31 +12,35 @@ import math
 
 text = """Ma3ry # wan$ted to swim! Ho*&wever, sh5e w#as afra<id of shar>ks."""
 punct_marks = ['.', '!', '?']
-undesired = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", '"', "@", "#", "$", "%", "^", "&", "(", ")", "<", ">", "+", "-", "=", "_", "*",","]
+undesired = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", '"', "@", "#", "$", "%", "^", "&", "(", ")", "<", ">", "+", "-", "=", "_", "*", ","]
 
 class WordStorage:
+
+    def __init__(self):
+       self.storage = {}
+
     def put(self, word: str) -> int:
-        for i in range(0, len(corpus)):
-            for k in range(0, len(corpus[i])):
-                if word == corpus[i][k] and word not in self.storage.keys():
-                   num = 0
-                   for n in range(0, len(corpus[0:i])):
-                       num += len(corpus[n])
-                   num = num + len(corpus[i][0:k])
-                   self.storage[word] = num
-                   return num
+        if word == None or type(word) != 'str' or word in self.storage.keys():
+           return {}
+        else:
+           self.storage[word] = num
+           return num
 
     def get_id_of(self, word: str) -> int:
-        if word in self.storage.keys():
+        if word in self.storage.keys() and word != '' and word != None and type(word) == "str":
             return self.storage[word]
+        elif word == '' or word == None or type(word) != "str":
+            return -1
         else:
             return None
 
-    def get_original_by(self, id: int) -> str:
-        if num in self.storage.values():
-            for k in self.storage.keys():
-                if self.storage[k] == num:
-                    return k
+    def get_original_by(self, num: int) -> str:
+        if num == None or type(num) != 'int' or num not in self.storage.values():
+           return 'UNK'
+        elif num in self.storage.values():
+           for k in self.storage.keys():
+               if self.storage[k] == num:
+                  return k
         else:
             return None
 
@@ -64,27 +68,28 @@ def encode(storage_instance, corpus) -> list:
 
 
 def split_by_sentence(text: str) -> list:
-    text = list(text)
-    text = list(filter(lambda x: x not in undesired, text))
-    text.remove(text[-1])
-    for i in range(1, len(text) - 1):
-        if text[i] == " " and text[i - 1] == " ":
-            text.remove(text[i])
-        if text[i] in punct_marks:
-            text[i] = '.'
-    text = ''.join(text)
-    text = text.split(".")
-    for i in range(0, len(text)):
-        if text[i].isalpha:
-            text[i] = text[i].lower()
-        text[i] = text[i].split(" ")
-        for k in range(0, len(text[i]) - 1):
-            if text[i][k] == '':
-                text[i].remove(text[i][k])
-            text[i][k].lower()
-        text[i].insert(0, "<s>")
-        text[i].insert(len(text[i]), "</s>")
+    if text == '' or text == None:
+       return []
+    else:
+       text = list(text)
+       text = list(filter(lambda x: x not in undesired, text))
+       text.remove(text[-1])
+       for i in range(1, len(text) - 1):
+           if text[i] == " " and text[i - 1] == " ":
+              text.remove(text[i])
+           if text[i] in punct_marks:
+              text[i] = '.'
+       text = ''.join(text)
+       text = text.split(".")
+       for i in range(0, len(text)):
+           if text[i].isalpha:
+              text[i] = text[i].lower()
+           text[i] = text[i].split(" ")
+           for k in range(0, len(text[i]) - 1):
+               if text[i][k] == '':
+                  text[i].remove(text[i][k])
+               text[i][k].lower()
+           text[i].insert(0, "<s>")
+           text[i].insert(len(text[i]), "</s>")
     return text
-
-corpus = tuple(split_by_sentence(text))
 
