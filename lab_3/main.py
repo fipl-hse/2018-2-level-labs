3c16a04da313e36588f4f3e01ab679fa8d330be3
admin <zadvornovk@mail.ru>
2018-12-03 00:58:10 +0300

    final version of the whole work

--- lab_3/main.py
+++ lab_3/main.py
@@ -5,12 +5,11 @@ Labour work #3
 
 import math
 
-#REFERENCE_TEXT = ''
-#if __name__ == '__main__':
-    #with open('not_so_big_reference_text.txt', 'r') as f:
-        #REFERENCE_TEXT = f.read()
+REFERENCE_TEXT = ''
+if __name__ == '__main__':
+    with open('not_so_big_reference_text.txt', 'r') as f:
+        REFERENCE_TEXT = f.read()
 
-text = """Ma3ry # wan$ted to swim! Ho*&wever, sh5e w#as afra<id of shar>ks."""
 punct_marks = ['.', '!', '?']
 undesired = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", '"', "@", "#", "$", "%", "^", "&", "(", ")", "<", ">", "+", "-", "=", "_", "*", ","]
 
@@ -66,30 +65,68 @@ class NGramTrie:
 
     def __init__(self, size):
        self.gram_frequencies = {}
-       self.log_probabilities = {}
+       self.gram_log_probabilities = {}
        self.size = size
 
     def fill_from_sentence(self, sentence: tuple) -> str:
         keys = []
-        if self.size == 2:
-           if sentence == () or type(sentence) != tuple or sentence == None:
-              self.gram_frequencies = {}
-           else:
-              for i in range(0, len(sentence)-1):
-                  k = tuple(sentence[i: i+2])
+        if sentence == () or type(sentence) != tuple or sentence == None:
+           self.gram_frequencies = {}
+        else:
+           for i in range(0, len(sentence)-(self.size-1)):
+               k = tuple(sentence[i: i + self.size])
+               keys.append(k)
+               if k[-1] == '</s>':
                   keys.append(k)
-                  if k[-1] == '</s>':
-                     keys.append(k)
-                     break
-              for i in range(len(keys)):
-                  self.gram_frequencies[keys[i]] = keys.count(keys[i])
-              return'OK'
+                  break
+           for i in range(len(keys)):
+               self.gram_frequencies[keys[i]] = keys.count(keys[i])
+           return'OK'
 
     def calculate_log_probabilities(self):
-        pass
+        zn = 0
+        for k in self.gram_frequencies.keys():
+            zn = self.gram_frequencies[k]
+            ch = self.gram_frequencies[k]
+            for key in self.gram_frequencies.keys():
+                if key != k and key[0:self.size-1] == k[0:self.size-1]:
+                   zn += self.gram_frequencies[key]
+            self.gram_log_probabilities[k] = math.log(ch/zn)
+        return self.gram_log_probabilities
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        pass
+        if prefix == None or type(prefix) != tuple:
+           return []
+        elif len(prefix) != self.size-1:
+           return []
+        else:
+           lst = []
+           for k in self.gram_log_probabilities.keys():
+               if k[0:self.size-1] == prefix:
+                  lst.append(self.gram_log_probabilities[k])
+           if lst == []:
+              return list(prefix)
+           else:
+               res = []
+               val = []
+               while True:
+                   for k in self.gram_log_probabilities.keys():
+                       if k[0:self.size-1] == prefix:
+                           val.append(self.gram_log_probabilities[k])
+                   if len(val) == 0:
+                       break
+                   else:
+                       maxi = max(val)
+                       for k in self.gram_log_probabilities.keys():
+                           if self.gram_log_probabilities[k] == maxi and res == []:
+                               for c in k:
+                                   res.append(c)
+                           elif self.gram_log_probabilities[k] == maxi and len(res) != 0:
+                               res.append(k[-1])
+                       val = []
+                       maxi = 0
+                       prefix = tuple(res[-(self.size-1):])
+               return res
 
 
 def encode(storage_instance, corpus) -> list:
@@ -100,33 +137,33 @@ def encode(storage_instance, corpus) -> list:
             corpus[i][k] = storage_instance[corpus[i][k]]
     return corpus
 
-def split_by_sentence(text: str) -> list:
-    if text == '' or text == None:
+def split_by_sentence(REFERENCE_TEXT: str) -> list:
+    if REFERENCE_TEXT == '' or REFERENCE_TEXT == None:
         return []
     else:
-        if "\n" in text:
-            text = text.replace("\n", " ")
-        while "  " in text:
-            text = text.replace("  ", " ")
-        text = list(text)
-        text = list(filter(lambda x: x not in undesired, text))
-        text.remove(text[-1])
-        for i in range(len(text)):
-            if text[i] in punct_marks:
-                text[i] = '.'
-        text = ''.join(text)
-        text = text.split(".")
-        for i in range(0, len(text)):
-            if text[i].isalpha:
-                text[i] = text[i].lower()
-        if len(text) == 1:
+        if "\n" in REFERENCE_TEXT:
+            REFERENCE_TEXT = REFERENCE_TEXT.replace("\n", " ")
+        while "  " in REFERENCE_TEXT:
+            REFERENCE_TEXT = REFERENCE_TEXT.replace("  ", " ")
+        REFERENCE_TEXT = list(REFERENCE_TEXT)
+        REFERENCE_TEXT = list(filter(lambda x: x not in undesired, REFERENCE_TEXT))
+        REFERENCE_TEXT.remove(REFERENCE_TEXT[-1])
+        for i in range(len(REFERENCE_TEXT)):
+            if REFERENCE_TEXT[i] in punct_marks:
+                REFERENCE_TEXT[i] = '.'
+        REFERENCE_TEXT  = ''.join(REFERENCE_TEXT)
+        REFERENCE_TEXT = REFERENCE_TEXT.split(".")
+        for i in range(0, len(REFERENCE_TEXT)):
+            if REFERENCE_TEXT[i].isalpha:
+               REFERENCE_TEXT[i] = REFERENCE_TEXT[i].lower()
+        if len(REFERENCE_TEXT) == 1:
             return []
-        for i in range(0, len(text)):
-            text[i] = text[i].split(" ")
-            for k in range(0, len(text[i]) - 1):
-                if text[i][k] == '':
-                    text[i].remove(text[i][k])
-                text[i][k].lower()
-            text[i].insert(0, "<s>")
-            text[i].insert(len(text[i]), "</s>")
-        return text
+        for i in range(0, len(REFERENCE_TEXT)):
+            REFERENCE_TEXT[i] = REFERENCE_TEXT[i].split(" ")
+            for k in range(0, len(REFERENCE_TEXT[i]) - 1):
+                if REFERENCE_TEXT[i][k] == '':
+                   REFERENCE_TEXT[i].remove(REFERENCE_TEXT[i][k])
+                REFERENCE_TEXT[i][k].lower()
+            REFERENCE_TEXT[i].insert(0, "<s>")
+            REFERENCE_TEXT[i].insert(len(REFERENCE_TEXT[i]), "</s>")
+        return REFERENCE_TEXT
