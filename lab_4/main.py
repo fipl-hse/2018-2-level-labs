278911841f9595dbc686f8e449b7a1945a365572
alinaalborova <43433331+alinaalborova@users.noreply.github.com>
2018-12-17 19:42:33 +0300

    started anew. added the first 2 functions

--- lab_4/main.py
+++ lab_4/main.py
@@ -9,68 +9,90 @@ if __name__ == '__main__':
 
 
 def clean_tokenize_corpus(texts):
-  reviews = []  # a list of texts to clean
+      try:
+        reviews = []
+        paragraphs = []
+        for t in texts:
+            try:
+                paragraph = t.split('<br /><br />')
+                paragraphs.append(paragraph)
+            except:
+                paragraphs.append([])
 
-  paragraphs = []  # getting rid of the break signs between paragraphs
-  try:
-      for t in texts:
-          if type(t) is not str:
-              texts.remove(t)
-  except:
-      return []
-  try:
-      for t in texts:
-          try:
-              paragraph = t.split('<br /><br />')
-              paragraphs.append(paragraph)
-          except:
-              paragraphs = []
 
-      raw_reviews = []  #joining them back
-      for p in paragraphs:
-          try:
-              raw_review = ' '.join(p)
-              raw_reviews.append(raw_review)
-          except:
-              raw_reviews = []
+        raw_reviews = []
+        for p in paragraphs:
+            raw_review = ' '.join(p)
+            raw_reviews.append(raw_review)
+       
+        for r_r in raw_reviews:
+            review = r_r.split(' ')
+            reviews.append(review)
 
-      for r_r in raw_reviews:
-          try:
-              review = r_r.split(' ')
-              reviews.append(review)  #getting a list of lists
-          except:
-              reviews = []
-
-      clean_reviews = []
-      for r in reviews:
-          clean_review = []
-          for w in r:
-              try:
-                  w = w.lower()
-                  word = ''  #clean word iterable
-              except:
-                  word = ''
-              try:
-                  for symbol in w:
-                      if symbol.isalpha():
-                          word += symbol
-                  if word != '':
-                      clean_review.append(word)
-              except:
-                  clean_review = []
-          clean_reviews.append(clean_review)
-  except:
-      clean_reviews = []
-  return clean_reviews
+        clean_reviews = []
+        for review in reviews:
+            clean_review = []
+            for w in review:
+                w = w.lower()
+                word = ''
+                for symbol in w:
+                    if symbol.isalpha():
+                        word += symbol
+                if word != '':
+                    clean_review.append(word)
+            if clean_review != []:
+                clean_reviews.append(clean_review)
+    except:
+        clean_reviews = []
+    return clean_reviews
 
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = []
+        self.tf_idf_values = []
 
     def calculate_tf(self):
-        pass
+        try:
+            words_quantity = []
+            no_duplicates_corpus = []
+            for text in self.corpus:
+                num_of_words = 0
+                no_duplicates_text = []
+                try:
+                    for word in text:
+                        if type(word) is str:
+                            num_of_words += 1
+                        if word not in no_duplicates_text and word != '' and type(word) is str:
+                            no_duplicates_text.append(word)
+                except:
+                    no_duplicates_text = []
+                no_duplicates_corpus.append(no_duplicates_text)
+                words_quantity.append(num_of_words)
+
+            frequencies = []
+            for ind, text in enumerate(no_duplicates_corpus):
+                frequencies_review = []
+                for token in text:
+                    i = 0
+                    while i < len(self.corpus):
+                        frequency = self.corpus[ind].count(token)
+                        i += 1
+                    tf = frequency/words_quantity[ind]
+                    frequencies_review.append(tf)
+                frequencies.append(frequencies_review)
+            tf_for_each_text = []
+            for index, text in enumerate(no_duplicates_corpus):
+                dictionary = dict(zip(no_duplicates_corpus[index], frequencies[index]))
+                if dictionary != {}:
+                    tf_for_each_text.append(dictionary)
+        except:
+            tf_for_each_text = []
+        self.tf_values = tf_for_each_text
+
 
     def calculate_idf(self):
         pass
