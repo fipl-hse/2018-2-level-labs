ad7260e514ef1b3cd05b3ba65aa99159be1a92f8
anasultanova <anastasia.sultanova@yandex.ru>
2018-12-18 16:02:57 +0300

    Done something, but there are mistakes

--- lab_4/main.py
+++ lab_4/main.py
@@ -9,24 +9,111 @@ if __name__ == '__main__':
 
 
 def clean_tokenize_corpus(texts: list) -> list:
-    pass
+    if not texts or not isinstance(texts, list):
+        return []
+    token_corpus = []
+    for text in texts:
+        if text and isinstance(text, str):
+            while '<br />' in text:
+                text = text.replace("<br />", " ")
+            token = []
+            words = text.split(" ")
+            for word in words:
+                new_word = ""
+                if not word.isalpha():
+                    word = word.lower()
+                    for i in word:
+                        if i.isalpha():
+                            new_word += i
+                    if new_word:
+                        token.append(new_word)
+                else:
+                    token.append(word)
+            token_corpus += token
+    return token_corpus
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
 
     def calculate_tf(self):
-        pass
+        if not isinstance(self.corpus, list):
+            return []
+        for text in self.corpus:
+            if not isinstance(text, list):
+                continue
+            tf_dict = {}
+            corpus_length = len(text)
+            for word in text:
+                if type(word) != str:
+                    corpus_length -= 1
+                if not isinstance(word, str):
+                    continue
+                freq = 0
+                for word2 in text:
+                    if word2 == word:
+                        freq += 1
+                    else:
+                        continue
+                tf_dict[word] = freq / corpus_length
+            self.tf_values.append(tf_dict)
+
 
     def calculate_idf(self):
-        pass
+        if self.corpus is None:
+            return {}
+        num_of_texts = len(self.corpus)
+        for text in self.corpus:
+            if type(text) != list:
+                num_of_texts -= 1
+        for text in self.corpus:
+            if not isinstance(text, list):
+                continue
+            for word in text:
+                if not isinstance(word, str):
+                    continue
+                num_of_word = 0
+                for text2 in self.corpus:
+                    if not isinstance(text2, list):
+                        continue
+                    if word in text2:
+                        num_of_word += 1
+                self.idf_values[word] = math.log(num_of_texts / num_of_word)
 
     def calculate(self):
-        pass
+        if self.tf_values is None or self.idf_values is None:
+            return []
+        if self.idf_values == {} or self.tf_values == []:
+            return []
+        for tf_dict in self.tf_values:
+            tf_idf = {}
+            for word in tf_dict:
+                tf_idf[word] = tf_dict[word] * self.idf_values[word]
+            self.tf_idf_values.append(tf_idf)
 
     def report_on(self, word, document_index):
-        pass
+        if document_index > len(self.corpus) - 1:
+            return ()
+        if self.tf_idf_values == [] or self.tf_idf_values is None:
+            return ()
+        report_on_word = []
+        text = self.corpus[document_index]
+        dict_tf_idf = self.tf_idf_values[document_index]
+        lst = list()
+        for token in text:
+            if token in dict_tf_idf:
+                lst.append((dict_tf_idf[token], token))
+        lst.sort(reverse=True)
+        for element in lst:
+            if word == element[1]:
+                tf_idf_value = element[0]
+                position = lst.index(element)
+                report_on_word.append((tf_idf_value, position))
+        return report_on_word[0]
 
 
 # scenario to check your work
