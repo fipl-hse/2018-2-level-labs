3fe4235c3245c8a0a163bb2c5e94fa0fe1fafff3
katvyaz <43420873+katvyaz@users.noreply.github.com>
2018-12-18 20:41:40 +0300

    added code for lab 4

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,3 +1,4 @@
+import re
 import math
 
 REFERENCE_TEXTS = []
@@ -9,24 +10,108 @@ if __name__ == '__main__':
 
 
 def clean_tokenize_corpus(texts: list) -> list:
-    pass
+    if not texts:
+        return []
+
+    for text in texts:
+        if text is None or not isinstance(text, str):
+            texts.remove(text)
+
+    clean_texts = []
+
+    for text in texts:
+        if isinstance(text, str):
+            text = re.sub(r'\n', '', text)
+            text = re.sub(r'<br />', ' ', text)
+            text = re.sub(r'[,!:;`~@#$%^&*+=''><()-]', '', text)
+            clean_texts.append(text)
+
+    clean_tokenized_texts = []
+    for text in clean_texts:
+        token = re.split(r'\W+', text)
+        clean_tokenized_texts.append(token)
+
+    final_corpus = []
+    for text in clean_tokenized_texts:
+        new_text = [token.lower() for token in text]
+        final_corpus.append(new_text)
+
+    # delete last empty list
+    for text in final_corpus:
+        while '' in text:
+            text.remove('')
+
+    return final_corpus
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
 
     def calculate_tf(self):
-        pass
+        if self.corpus and isinstance(self.corpus, list):
+            for part in self.corpus:
+                if part is not None:
+                    len_of_text = 0
+                    freq_dict = {}
+                    for word in part:
+                        if isinstance(word, str):
+                            len_of_text += 1
+                            if word not in freq_dict:
+                                freq_dict[word] = 1
+                            else:
+                                new_word = freq_dict[word] + 1
+                                freq_dict[word] = new_word
+
+                    for word, freq in freq_dict.items():
+                        freq_dict[word] = freq / len_of_text
+
+                    self.tf_values.append(freq_dict)
 
     def calculate_idf(self):
-        pass
+        if self.corpus and isinstance(self.corpus, list):
+            len_of_corpus = 0
+            for part in self.corpus:
+                if isinstance(part, list):
+                    len_of_corpus += 1
+            for part in self.corpus:
+                if part is not None:
+                    for word in part:
+                        if isinstance(word, str):
+                            if word not in self.idf_values.keys():
+                                occurrence_counter = 0
+                                for text_to_compare in self.corpus:
+                                    if text_to_compare:
+                                        if word in text_to_compare:
+                                            occurrence_counter += 1
+                                self.idf_values[word] = math.log(len_of_corpus / occurrence_counter)
 
     def calculate(self):
-        pass
+        if self.tf_values and self.idf_values:
+            for tf_dict in self.tf_values:
+                particular_dict = {}
+                for word, tf_value in tf_dict.items():
+                    if self.idf_values[word]:
+                        particular_dict[word] = tf_value * self.idf_values[word]
+                    else:
+                        particular_dict[word] = 0
+                self.tf_idf_values.append(particular_dict)
 
     def report_on(self, word, document_index):
-        pass
+        final_tuple = tuple()
+        if not self.tf_idf_values or document_index >= len(self.tf_idf_values):
+            return ()
+        else:
+            current_tf_idf = self.tf_idf_values[document_index].get(word)
+            place = 0
+            if current_tf_idf is not None:
+                for one_tf_idf in self.tf_idf_values[document_index].values():
+                    if current_tf_idf < one_tf_idf:
+                        place += 1
+            return current_tf_idf, place
 
 
 # scenario to check your work
