8c7ed4fdbcfb000f1f037ad6bca508f894b56dec
amsuntsova <amsuntsova@edu.hse.ru>
2018-12-20 15:07:43 +0300

    corpus tokenizer added

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,5 +1,6 @@
 import math
 
+undesired = list("1234567890/?<>,.()-+=&*^:;%$#@!'""")
 REFERENCE_TEXTS = []
 if __name__ == '__main__':
     texts = ['5_7.txt', '15_2.txt', '10547_3.txt', '12230_7.txt']
@@ -7,9 +8,34 @@ if __name__ == '__main__':
         with open(text, 'r') as f:
             REFERENCE_TEXTS.append(f.read())
 
+def clean_tokenize_corpus(REFERENCE_TEXTS ) -> list:
+    corpus = []
+    if REFERENCE_TEXTS is None:
+        corpus = []
+    else:
+        REFERENCE_TEXTS = list(filter(lambda x: x is not None and type(x) == str, REFERENCE_TEXTS))
+        if REFERENCE_TEXTS == []:
+            corpus = []
+        else:
+            for el in REFERENCE_TEXTS:
+                if "\n" in el:
+                    el = el.replace("\n", " ")
+                if "<br />" in el:
+                    el = el.replace("<br />", " ")
+                while "  " in el:
+                    el = el.replace("  ", " ")
+                el = list(el)
+                el = list(filter(lambda x: x not in undesired, el))
+                for i in range(len(el)):
+                    el[i] = el[i].lower()
+                el = ''.join(el)
+                el = el.split(" ")
+                corpus.append(el)
+                for i in range(len(corpus)):
+                    corpus[i] = list(filter(lambda x: x != '', corpus[i]))
+    return corpus
 
-def clean_tokenize_corpus(texts: list) -> list:
-    return{"Ann":"goddess"}
+
 
 
 class TfIdfCalculator:
