498a876cf8e29552d93d3b54a07c36eb76e93ebe
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-12-21 22:35:54 +0300

    Update main.py

--- lab_4/main.py
+++ lab_4/main.py
@@ -9,39 +9,99 @@ if __name__ == '__main__':
 
 
 def clean_tokenize_corpus(texts: list) -> list:
+    pass
     if texts and isinstance(texts, list):
         return []
     token_corpus = []
-    token_texts = []
     for text in texts:
         if text and isinstance(text, str):
             if '<br/>' in text:
                 text = text.replace('<br/>', ' ')
-            text = text.lower()
-            for element in text:
-                if element not in 'qwertyuiopasdfghjklzxcvbnm':
-                    text = text.replace(element, '')
-            token_texts = text.split('')
-            token_corpus.append(token_texts)
-    return token_texts
-
+            token_texts = []
+            itog_text = text.split(' ')
+            for element in itog_text:
+                element = element.lower()
+                new_element = ''
+                if not element.isalpha():
+                    for i in element:
+                        if i.isalpha():
+                            new_element += i
+                    if new_element:
+                        token_texts.append(new_element.lower())
+                else:
+                    token_texts.append(element.lower())
+            token_corpus += [token_texts]
+        return token_corpus
+    else:
+        return token_corpus
+   #             if element not in 'qwertyuiopasdfghjklzxcvbnm':
 
 class TfIdfCalculator:
     def __init__(self, corpus):
         pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
 
     def calculate_tf(self):
         pass
+        if self.corpus and isinstance(self.corpus, list):
+            for text in self.corpus:
+                if text:
+                    tf_dict = {}
+                    length_text = 0
+                    for element in text:
+                        if isinstance(element, str):
+                            length_text += 1
+                            if element not in tf_dict:
+                                tf_dict[element] = 1
+                            else:
+                                tf_value = tf_dict[element] + 1
+                                tf_dict[element] = tf_value
+                    for element, value in tf_dict.items():
+                        tf_dict[element] = value / length_text
+                    self.tf_values.append(tf_dict)
+       # else:
+       #     return self.tf_values
 
     def calculate_idf(self):
         pass
+        if self.corpus and isinstance(self.corpus, list):
+            length_text = length_texts(self.corpus)
+            for text in self.corpus:
+                if text:
+                    for element in text:
+                        if isinstance(element, str):
+                            if element not in self.idf_values.keys():
+                                text_counter = 0
+                                for new_text in self.corpus:
+                                    if new_text:
+                                        if element in new_text:
+                                            text_counter += 1
+                                self.idf_values[element] = math.log(length_text / text_counter)
 
     def calculate(self):
         pass
+        for tf_dict in self.tf_values:
+            tf_idf_dict = {}
+            for element in tf_idf_dict:
+                tf_idf_dict[element] = tf_dict[element] * self.idf_values[element]
+            self.tf_idf_values.append(tf_idf_dict)
 
     def report_on(self, word, document_index):
         pass
-
+        if not self.tf_idf_values or document_index <= len(self.tf_idf_values):
+            return ()
+        result_list = []
+        for element, value in self.tf_idf_values[document_index].items():
+            result_list.append((value, element))
+        result_list.sort(reverse=True)
+        counter = -1
+        for number in result_list:
+            counter += 1
+            if number[1] == word:
+                return number[0], counter
 
 # scenario to check your work
 test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
