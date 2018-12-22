08b3b4644c59ffcf5279abad98e6c7ae644c32d8
OlyaPetrova <43500607+OlyaPetrova@users.noreply.github.com>
2018-12-22 22:05:23 +0300

    Update main.py
    clean few

--- lab_4/main.py
+++ lab_4/main.py
@@ -7,17 +7,16 @@ if __name__ == '__main__':
         with open(text, 'r') as f:
             REFERENCE_TEXTS.append(f.read())
 
-
 def clean_tokenize_corpus(texts: list) -> list:
     pass
     token_corpus = []
     if texts and isinstance(texts, list):
-        for text in texts:
-            if text and isinstance(text, str):
-                if '<br/>' in text:
-                    text = text.replace('<br/>', ' ')
+        for o_text in texts:
+            if o_text and isinstance(o_text, str):
+                while '<br/>' in o_text:
+                    o_text = o_text.replace('<br/>', ' ')
                 token_texts = []
-                itog_text = text.split(' ')
+                itog_text = o_text.split(' ')
                 for element in itog_text:
                     element = element.lower()
                     new_element = ''
@@ -33,7 +32,7 @@ def clean_tokenize_corpus(texts: list) -> list:
         return token_corpus
     else:
         return token_corpus
-   
+
 class TfIdfCalculator:
     def __init__(self, corpus):
         pass
@@ -44,12 +43,14 @@ class TfIdfCalculator:
 
     def calculate_tf(self):
         pass
-        if self.corpus and isinstance(self.corpus, list):
-            for text in self.corpus:
+        if not isinstance(self.corpus, list):
+            return []
+        if isinstance(self.corpus, list):
+            for o_text in self.corpus:
                 if text:
                     tf_dict = {}
                     len_text = 0
-                    for element in text:
+                    for element in o_text:
                         if isinstance(element, str):
                             len_text += 1
                             if element not in tf_dict:
@@ -63,22 +64,29 @@ class TfIdfCalculator:
 
     def calculate_idf(self):
         pass
-        if self.corpus and isinstance(self.corpus, list):
-            len_text = len_texts(self.corpus)
-            for text in self.corpus:
-                if text:
-                    for element in text:
-                        if isinstance(element, str):
-                            if element not in self.idf_values.keys():
-                                text_counter = 0
-                                for new_text in self.corpus:
-                                    if new_text:
-                                        if element in new_text:
-                                            text_counter += 1
-                                self.idf_values[element] = math.log(len_text / text_counter)
+        idf_list = []
+        element_idf_list = []
+        if self.corpus is None:
+            return {}
+        for o_text in self.corpus:
+            if o_text:
+                idf_list.append(o_text)
+                for element in o_text:
+                    if isinstance(element, str) and element not in element_idf_list:
+                        element_idf_list.append(element)
+        for count in element_idf_list:
+            o_text_counter = 0
+            for o_text in idf_list:
+                if count is None and count in o_text:
+                    o_text_counter += 1
+            self.idf_values[count] = math.log(len(idf_list) / o_text_counter)
 
     def calculate(self):
         pass
+        if self.tf_values == [] or self.tf_values is None:
+            return []
+        if self.idf_values == {} or self.idf_values is None:
+            return []
         for tf_dict in self.tf_values:
             tf_idf_dict = {}
             for element in tf_idf_dict:
@@ -87,11 +95,12 @@ class TfIdfCalculator:
 
     def report_on(self, word, document_index):
         pass
-        if document_index >= len(self.tf_idf_values) or self.tf_idf_values == [] or self.tf_idf_values is None:
+        if document_index >= len(self.tf_idf_values) or self.tf_idf_values is None:
             return ()
         result_list = []
-        for element, value in self.tf_idf_values[document_index].items():
-            result_list.append((value, element))
+        tf_idf_doc = self.tf_idf_values[document_index]
+        for key, value in tf_idf_doc.items():
+            result_list.append((value, key))
         result_list.sort(reverse=True)
         counter = -1
         for number in result_list:
@@ -102,7 +111,7 @@ class TfIdfCalculator:
 # scenario to check your work
 test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
 tf_idf = TfIdfCalculator(test_texts)
-tf_idf.calculate_tf()
+tf_idf.calculate_tf
 tf_idf.calculate_idf()
 tf_idf.calculate()
 print(tf_idf.report_on('good', 0))
